b515dd884aee73e50de54fecfcd8d97eb0389c6b
William Sanders <william.sanders076@gmail.com>
2020-04-06 19:18:26 -0400

    CSharp solution to ReverseInteger

---
+++ ReverseInteger/CSharp/ReverseInteger/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ReverseInteger
+{
+    public class Solution
+    {
+        public int Reverse(int x)
+        {
+            var isNegative = x < 0;
+            var digits = new List<int>();
+
+            if (isNegative)
+            {
+                x *= -1;
+            }
+
+            while (x > 0)
+            {
+                digits.Add(x % 10);
+                x /= 10;
+            }
+
+            var result = 0;
+            var power = 1;
+            for (var i = digits.Count - 1; i >= 0; i--)
+            {
+                try
+                {
+                    result = checked(result + digits[i] * power);
+                }
+                catch (Exception)
+                {
+                    return 0;
+                }
+                
+                try
+                {
+                    power = checked(power * 10);
+                }
+                catch (Exception)
+                {
+                    if (i == 0)
+                    {
+                        if (isNegative)
+                        {
+                            return result *= -1;
+                        }
+                        return result;
+                    }
+                    return 0;
+                }
+            }
+
+            if (isNegative)
+            {
+                result *= -1;
+            }
+
+            return result;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution();
+
+            Console.WriteLine(solution.Reverse(123));
+            Console.WriteLine(solution.Reverse(-123));
+            Console.WriteLine(solution.Reverse(120));
+            Console.WriteLine(solution.Reverse(1));
+            Console.WriteLine(solution.Reverse(-1));
+            Console.WriteLine(solution.Reverse(-2147483412));
+            Console.WriteLine(solution.Reverse(1534236469));
+
+
+            Console.ReadKey();
+        }
+    }
+}
