c61d9d783aed83183aaa756f93d1a155ef2b9172
William Sanders <william.sanders076@gmail.com>
2020-04-11 17:06:54 -0400

    CSharp solution to ValidParentheses

---
+++ ValidParentheses/CSharp/ValidParentheses/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ValidParentheses
+{
+    public class Solution
+    {
+        private static readonly Dictionary<char, char> OpeningToClosing = new Dictionary<char, char>
+        {
+            {'(', ')' }, {'{', '}'}, {'[', ']'}
+        };
+
+        public bool IsValid(string s)
+        {
+            var openingChars = new Stack<char>();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                var currentChar = s[i];
+                if (OpeningToClosing.ContainsKey(currentChar))
+                {
+                    openingChars.Push(currentChar);
+                    continue;
+                }
+                
+                if (!openingChars.TryPop(out var lastOpeningChar))
+                {
+                    return false;
+                }
+
+                if (OpeningToClosing[lastOpeningChar] != currentChar)
+                {
+                    return false;
+                }
+            }
+
+            return openingChars.Count == 0;
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution();
+
+            Console.WriteLine(solution.IsValid(""));
+            Console.WriteLine(solution.IsValid("()"));
+            Console.WriteLine(solution.IsValid("()[]{}"));
+            Console.WriteLine(solution.IsValid("(]"));
+            Console.WriteLine(solution.IsValid("([)]"));
+            Console.WriteLine(solution.IsValid("([])"));
+            Console.WriteLine(solution.IsValid("([]){}[]"));
+            Console.WriteLine(solution.IsValid("([)"));
+            Console.WriteLine(solution.IsValid("(([]){})"));
+            Console.WriteLine(solution.IsValid("["));
+
+            Console.ReadKey();
+        }
+    }
+}
