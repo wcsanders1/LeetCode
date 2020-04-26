111a714cf5f66eef904cdfdae2ad76c82d9adcf7
William Sanders <william.sanders076@gmail.com>
2020-04-26 15:11:39 -0400

    CSharp solution to GroupAnagrams

---
+++ GroupAnagrams/CSharp/GroupAnagrams/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GroupAnagrams
+{
+    public class Solution
+    {
+        public IList<IList<string>> GroupAnagrams(string[] strs)
+        {
+            var anagrams = new Dictionary<string, List<string>>();
+            foreach (var word in strs)
+            {
+                var sorted = new string(word.OrderBy(c => c).ToArray());
+                if (!anagrams.ContainsKey(sorted))
+                {
+                    anagrams.Add(sorted, new List<string> { word });
+                }
+                else
+                {
+                    anagrams[sorted].Add(word);
+                }
+            }
+
+            var result = new List<IList<string>>();
+
+            foreach (var (_, anagramList) in anagrams)
+            {
+                result.Add(anagramList);
+            }
+
+            return result;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution();
+
+            var result1 = solution.GroupAnagrams(new string[] { "eat", "tea", "tan", "ate", "nat", "bat" });
+
+            Console.ReadKey();
+        }
+    }
+}
