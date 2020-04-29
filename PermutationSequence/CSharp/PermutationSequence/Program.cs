26391411364e24448754a062103c491c701207b4
William Sanders <william.sanders076@gmail.com>
2020-04-29 19:39:53 -0400

    CSharp solutions to SpiralMatrixII and PermutationSequence

---
+++ PermutationSequence/CSharp/PermutationSequence/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PermutationSequence
+{
+    public class Solution
+    {
+        public string GetPermutation(int n, int k)
+        {
+            k--;
+
+            var permutations = 1;
+            var factors = new int[n - 1];
+            var possibleDigits = new List<string>();
+            for (int i = 1; i <= n; i++)
+            {
+                possibleDigits.Add(i.ToString());
+                permutations *= i;
+                
+                if (i < n)
+                {
+                    factors[n - i - 1] = permutations;
+                }
+            }
+
+            if (k > permutations)
+            {
+                return string.Empty;
+            }
+
+            var result = new StringBuilder();
+            for (int i = 0; i < factors.Length; i++)
+            {
+                var index = k / factors[i];
+                result.Append(possibleDigits[index].ToString());
+                possibleDigits.RemoveAt(index);
+
+                k %= factors[i];
+            }
+
+            result.Append(possibleDigits[0]);
+            return result.ToString();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution();
+
+            Console.WriteLine(solution.GetPermutation(4, 9));
+            Console.WriteLine(solution.GetPermutation(8, 9876));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SpiralMatrixII/CSharp/SpiralMatrixII/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace SpiralMatrixII
+{
+    public class Solution
+    {
+        public int[][] GenerateMatrix(int n)
+        {
+            var result = new int[n][];
+            for (int i = 0; i < n; i++)
+            {
+                result[i] = new int[n];
+            }
+
+            var left = 0;
+            var top = 0;
+            var right = n;
+            var bottom = n;
+
+            var number = 1;
+
+            while (left < right && top < bottom)
+            {
+                for (var i = left; i < right; i++)
+                {
+                    result[top][i] = number++;
+                }
+
+                for (var i = top + 1; i < bottom; i++)
+                {
+                    result[i][right - 1] = number++;
+                }
+
+                if (top < bottom - 1 && left < right - 1)
+                {
+                    for (var i = right - 2; i >= left; i--)
+                    {
+                        result[bottom - 1][i] = number++;
+                    }
+
+                    for (var i = bottom - 2; i > top; i--)
+                    {
+                        result[i][left] = number++;
+                    }
+                }
+
+                left++;
+                top++;
+                right--;
+                bottom--;
+            }
+
+            return result;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution();
+
+            var result1 = solution.GenerateMatrix(3);
+            var result2 = solution.GenerateMatrix(4);
+            var result3 = solution.GenerateMatrix(5);
+            var result4 = solution.GenerateMatrix(0);
+            var result5 = solution.GenerateMatrix(1);
+            var result6 = solution.GenerateMatrix(2);
+
+            Console.ReadKey();
+        }
+    }
+}
