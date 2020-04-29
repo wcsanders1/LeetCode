f35587dc7597473cda15acea6cae9c30a811d18f
William Sanders <william.sanders076@gmail.com>
2020-04-28 20:30:45 -0400

    CSharp solution to LengthOfLastWord

---
+++ LengthOfLastWord/CSharp/LengthOfLastWord/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace LengthOfLastWord
+{
+    public class Solution
+    {
+        public int LengthOfLastWord(string s)
+        {
+            var result = 0;
+
+            for (var i = s.Length - 1; i >= 0; i--)
+            {
+                if (s[i] == ' ')
+                {
+                    if (result > 0)
+                    {
+                        return result;
+                    }
+                }
+                else
+                {
+                    result++;
+                }
+            }
+
+            return result;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution();
+
+            Console.WriteLine(solution.LengthOfLastWord("Hello World"));
+            Console.WriteLine(solution.LengthOfLastWord("Hello World     "));
+            Console.WriteLine(solution.LengthOfLastWord("HelloWorld"));
+            Console.WriteLine(solution.LengthOfLastWord("     "));
+            Console.WriteLine(solution.LengthOfLastWord("e     "));
+
+            Console.ReadKey();
+        }
+    }
+}
