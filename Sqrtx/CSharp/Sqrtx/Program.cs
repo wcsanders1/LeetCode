2d935121e0a922093f387b8d883225a77f440f14
William Sanders <william.sanders076@gmail.com>
2020-05-18 06:24:03 -0400

    CSharp solution to Sqrtx

---
+++ Sqrtx/CSharp/Sqrtx/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Sqrtx
+{
+    public class Solution
+    {
+        public int MySqrt(int x)
+        {
+            var comp = (ulong)x;
+            var result = comp;
+            while (result * result > comp)
+            {
+                result /= 2;
+            }
+
+            while ((result + 1) * (result + 1) <= comp)
+            {
+                result++;
+            }
+
+            return (int)result;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution();
+
+            Console.WriteLine(solution.MySqrt(4));
+            Console.WriteLine(solution.MySqrt(8));
+            Console.WriteLine(solution.MySqrt(400));
+            Console.WriteLine(solution.MySqrt(147));
+            Console.WriteLine(solution.MySqrt(2025));
+            Console.WriteLine(solution.MySqrt(2147395599));
+
+            Console.ReadKey();
+        }
+    }
+}
