3568bb11e919cc8044dea917da33179e089946d9
William Sanders <william.sanders076@gmail.com>
2020-07-09 20:45:01 -0400

    Unfinished CSharp solution to ScrambleString

---
+++ ScrambleString/CSharp/ScrambleString/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ScrambleString
+{
+    // NOT DONE
+    public class Solution
+    {
+        public bool IsScramble(string s1, string s2)
+        {
+            if (string.IsNullOrEmpty(s1))
+            {
+                return false;
+            }
+
+            if (s1.Length == 1)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
