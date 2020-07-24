832a3b0b4ab0aaaf2a20354d8727b1e6538c4454
William Sanders <william.sanders076@gmail.com>
2020-07-23 21:22:17 -0400

    CSharp solution to MaxDotProductOfTwoSubsequences

---
+++ MaxDotProductOfTwoSubsequences/CSharp/MaxDotProductOfTwoSubsequences/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace MaxDotProductOfTwoSubsequences
+{
+    public class Solution
+    {
+        public int MaxDotProduct(int[] nums1, int[] nums2)
+        {
+            var dp = new int[nums1.Length][];
+
+            for (int i = 0; i < nums1.Length; i++)
+            {
+                dp[i] = new int[nums2.Length];
+
+                for (int j = 0; j < nums2.Length; j++)
+                {
+                    dp[i][j] = nums1[i] * nums2[j];
+
+                    if(i > 0 && j > 0)
+                    {
+                        dp[i][j] += Math.Max(dp[i - 1][j - 1], 0);
+                    }
+
+                    if (i > 0)
+                    {
+                        dp[i][j] = Math.Max(dp[i][j], dp[i - 1][j]);
+                    }
+
+                    if (j > 0)
+                    {
+                        dp[i][j] = Math.Max(dp[i][j], dp[i][j - 1]);
+                    }
+                }
+            }
+
+            return dp[nums1.Length - 1][nums2.Length - 1];
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution();
+
+            Console.WriteLine(solution.MaxDotProduct(new int[] { 2, 1, -2, 5 }, new int[] { 3, 0, -6 }));
+
+            Console.ReadKey();
+        }
+    }
+}
