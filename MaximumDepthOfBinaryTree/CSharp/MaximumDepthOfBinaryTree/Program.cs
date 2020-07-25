f92f39049995423d36c2c78bbf872c2d72f71e93
William Sanders <william.sanders076@gmail.com>
2020-07-25 10:50:55 -0400

    CSharp solution to MaximumDepthOfBinaryTree

---
+++ MaximumDepthOfBinaryTree/CSharp/MaximumDepthOfBinaryTree/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace MaximumDepthOfBinaryTree
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public class Solution
+    {
+        public int MaxDepth(TreeNode root)
+        {
+            return GetMaxDepth(root);
+        }
+
+        private int GetMaxDepth(TreeNode node, int currentDepth = 0)
+        {
+            if (node == null)
+            {
+                return currentDepth;
+            }
+
+            currentDepth++;
+
+            var leftDepth = GetMaxDepth(node.left, currentDepth);
+            var rightDepth = GetMaxDepth(node.right, currentDepth);
+
+            return Math.Max(leftDepth, rightDepth);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadKey();
+        }
+    }
+}
