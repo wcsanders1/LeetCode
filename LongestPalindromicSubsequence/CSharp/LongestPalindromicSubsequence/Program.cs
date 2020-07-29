6657f43a6e868030dc279883f40301b51dcf8c7c
William Sanders <william.sanders076@gmail.com>
2020-07-29 19:18:15 -0400

    CSharp solution to LongestPalindromicSubsequence

---
+++ LongestPalindromicSubsequence/CSharp/LongestPalindromicSubsequence/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace LongestPalindromicSubsequence
+{
+    public class Solution
+    {
+        public int LongestPalindromeSubseq(string s)
+        {
+            if (string.IsNullOrWhiteSpace(s))
+            {
+                return 0;
+            }
+
+            var dp = new int[s.Length + 1][];
+            for (int i = 0; i <= s.Length; i++)
+            {
+                dp[i] = new int[s.Length + 1];
+            }
+
+            for (int col = 1; col <= s.Length; col++)
+            {
+                var reverseIndex = s.Length - 1;
+                for (int row = 1; row <= s.Length; row++)
+                {
+                    if (s[col - 1] == s[reverseIndex--])
+                    {
+                        dp[col][row] = dp[col - 1][row - 1] + 1;
+                    }
+                    else
+                    {
+                        dp[col][row] = Math.Max(dp[col - 1][row], dp[col][row - 1]);
+                    }
+                }
+            }
+
+            return dp[s.Length][s.Length];
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution();
+
+            Console.WriteLine(solution.LongestPalindromeSubseq("bbbab"));
+            Console.WriteLine(solution.LongestPalindromeSubseq("cbbd"));
+
+            Console.ReadKey();
+        }
+    }
+}
