2a298a1d5983c6d493a19fa7860f6bb3ce9469a1
William Sanders <william.sanders076@gmail.com>
2020-08-03 19:33:25 -0400

    CSharp solution to UniqueSubstringsInWraparoundString

--- UniqueSubstringsInWraparoundString/CSharp/UniqueSubstringsInWraparoundString/Program.cs
+++ UniqueSubstringsInWraparoundString/CSharp/UniqueSubstringsInWraparoundString/Program.cs
@@ -1,12 +1,51 @@
 ﻿using System;
+using System.Linq;
 
 namespace UniqueSubstringsInWraparoundString
 {
+    public class Solution
+    {
+        public int FindSubstringInWraproundString(string p)
+        {
+            if (string.IsNullOrWhiteSpace(p))
+            {
+                return 0;
+            }
+
+            var results = new int[26];
+
+            var currentMax = 0;
+            for (int i = 0; i < p.Length; i++)
+            {
+                if (i > 0 && (p[i] - p[i - 1] == 1 || p[i - 1] - p[i] == 25))
+                {
+                    currentMax++;
+                }
+                else
+                {
+                    currentMax = 1;
+                }
+
+                var index = p[i] - 'a';
+
+                results[index] = Math.Max(results[index], currentMax);
+            }
+
+            return results.Sum();
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var solution = new Solution();
+
+            Console.WriteLine(solution.FindSubstringInWraproundString("a"));
+            Console.WriteLine(solution.FindSubstringInWraproundString("cac"));
+            Console.WriteLine(solution.FindSubstringInWraproundString("zab"));
+
+            Console.ReadKey();
         }
     }
 }
