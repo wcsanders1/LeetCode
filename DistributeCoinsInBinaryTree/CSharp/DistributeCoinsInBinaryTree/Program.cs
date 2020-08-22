b1ba992086ce61d1e0406ae6a84fc9f81dfb2d39
William Sanders <william.sanders076@gmail.com>
2020-08-22 11:04:18 -0400

    Scaffold CSharp solution to DistributeCoinsInBinaryTree

---
+++ DistributeCoinsInBinaryTree/CSharp/DistributeCoinsInBinaryTree/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace DistributeCoinsInBinaryTree
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public class Solution
+    {
+        public int DistributeCoins(TreeNode root)
+        {
+            return 0;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
