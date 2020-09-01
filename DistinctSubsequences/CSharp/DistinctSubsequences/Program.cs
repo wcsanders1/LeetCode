88aac4391c96e98e89914f483d58ef5aad7f927b
William Sanders <william.sanders076@gmail.com>
2020-09-01 07:01:08 -0400

    CSharp solution to DistinctSubsequences

---
+++ DistinctSubsequences/CSharp/DistinctSubsequences/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace DistinctSubsequences
+{
+    public class Solution
+    {
+        public int NumDistinct(string s, string t)
+        {
+            var dp = new int[t.Length + 1][];
+            for (int i = 0; i <= t.Length; i++)
+            {
+                dp[i] = new int[s.Length + 1];
+            }
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                dp[0][i] = 1;
+            }
+
+            for (int i = 0; i < t.Length; i++)
+            {
+                for (int j = 0; j < s.Length; j++)
+                {
+                    if (t[i] == s[j])
+                    {
+                        dp[i + 1][j + 1] = dp[i][j] + dp[i + 1][j];
+                    }
+                    else
+                    {
+                        dp[i + 1][j + 1] = dp[i + 1][j];
+                    }
+                }
+            }
+
+            return dp[t.Length][s.Length];
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution();
+
+            Console.WriteLine(solution.NumDistinct("rabbbit", "rabbit"));
+            Console.WriteLine(solution.NumDistinct("babgbag", "bag"));
+            Console.WriteLine(solution.NumDistinct("ccc", "c"));
+            Console.WriteLine(solution.NumDistinct("ccc", "cc"));
+
+            Console.ReadKey();
+        }
+    }
+}
