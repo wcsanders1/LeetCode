0f873f4e4d5b0214b42a2091cb453c9dbbeb5c3c
William Sanders <william.sanders076@gmail.com>
2020-09-05 14:46:03 -0400

    Scaffold CSharp solution to KSimilarStrings

---
+++ KSimilarStrings/CSharp/KSimilarStrings/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace KSimilarStrings
+{
+    public class Solution
+    {
+        public int KSimilarity(string A, string B)
+        {
+            
+
+
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
