59b69627856a30c0b8e843096d8dd3a66edad96a
William Sanders <william.sanders076@gmail.com>
2020-09-12 17:52:35 -0400

    CSharp solution to BusRoutes

---
+++ BusRoutes/CSharp/BusRoutes/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BusRoutes
+{
+    public class Solution
+    {
+        public int NumBusesToDestination(int[][] routes, int S, int T)
+        {
+            var graph = new Dictionary<int, List<int>>();
+            foreach (var route in routes)
+            {
+                var distinctRoutes = route.Distinct().ToArray();
+                for (var i = 0; i < distinctRoutes.Length; i++)
+                {
+                    var stop = distinctRoutes[i];
+                    if (!graph.ContainsKey(stop))
+                    {
+                        graph.Add(stop, new List<int>());
+                    }
+
+                    graph[stop].AddRange(distinctRoutes.Where((r, j) => j != i).ToList());
+                }
+            }
+
+            return GetNumBuses(graph, S, T);
+        }
+
+        private int GetNumBuses(Dictionary<int, List<int>> graph, int S, int T)
+        {
+            if (S == T)
+            {
+                return 0;
+            }
+
+            var queue = new Queue<(int, int)>();
+            queue.Enqueue((S, 1));
+            var visited = new HashSet<int>();
+
+            while (queue.Count > 0)
+            {
+                var (stop, busses) = queue.Dequeue();
+                var stops = graph[stop];
+
+                if (!visited.Contains(stop))
+                {
+                    visited.Add(stop);
+                }
+
+                foreach (var nextStop in stops)
+                {
+                    if (nextStop == T)
+                    {
+                        return busses;
+                    }
+
+                    if (!visited.Contains(nextStop))
+                    {
+                        visited.Add(nextStop);
+                        queue.Enqueue((nextStop, busses + 1));
+                    }
+                }
+            }
+
+            return -1;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution();
+
+            Console.WriteLine(solution.NumBusesToDestination(new int[][]
+            {
+                new int [] {1, 2, 7 },
+                new int [] {3, 6, 7}
+            }, 1, 6));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MissingNumber/CSharp/MissingNumber/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace MissingNumber
+{
+    public class Solution
+    {
+        public int MissingNumber(int[] nums)
+        {
+            var expectedTotal = 0;
+            for (int i = 1; i <= nums.Length; i++)
+            {
+                expectedTotal += i;
+            }
+
+            return expectedTotal - nums.Sum();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution();
+
+            Console.WriteLine(solution.MissingNumber(new int[] { 3, 0, 1 }));
+            Console.WriteLine(solution.MissingNumber(new int[] { 9, 6, 4, 2, 3, 5, 7, 0, 1 }));
+
+            Console.ReadKey();
+        }
+    }
+}
