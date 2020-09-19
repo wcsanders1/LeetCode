af1a43b0afd7df2d23aeec4bd458c362e5f22f66
William Sanders <william.sanders076@gmail.com>
2020-09-19 12:25:00 -0400

    CSharp solution to Triangle

---
+++ Triangle/CSharp/Triangle/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Triangle
+{
+    public class Solution
+    {
+        public int MinimumTotal(IList<IList<int>> triangle)
+        {
+            if (triangle == null || triangle.Count == 0)
+            {
+                return 0;
+            }
+
+            if (triangle.Count == 1)
+            {
+                return triangle[0][0];
+            }
+
+            for (int i = triangle.Count - 2; i >= 0; i--)
+            {
+                var currentRow = triangle[i];
+                var lowerRow = triangle[i + 1];
+
+                for (int j = 0; j < currentRow.Count; j++)
+                {
+                    var currentNum = currentRow[j];
+                    currentRow[j] = Math.Min(currentNum + lowerRow[j], currentNum + lowerRow[j + 1]);
+                }
+            }
+
+            return triangle[0][0];
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution();
+
+            var triangleOne = new List<IList<int>>
+            {
+                new List<int>{2},
+                new List<int>{3, 4,},
+                new List<int>{6, 5, 7},
+                new List<int>{4, 1, 8, 3}
+            };
+
+            Console.WriteLine(solution.MinimumTotal(triangleOne));
+
+            Console.ReadKey();
+        }
+    }
+}
