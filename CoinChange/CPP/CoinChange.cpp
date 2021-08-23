7b498c7bc134d66887386b185d7be88ee44a383b
William Sanders <william.sanders076@gmail.com>
2021-08-23 06:55:11 -0400

    CoinChange solution

--- CoinChange/CPP/CoinChange.cpp
+++ CoinChange/CPP/CoinChange.cpp
@@ -8,55 +8,22 @@ class Solution
 public:
   int coinChange(vector<int> &coins, int amount)
   {
-    sort(coins.begin(), coins.end());
-    vector<long> *remainders = new vector<long>();
-    remainders->push_back(0);
-    
-    for (long i = 1; i <= coins.back(); i++)
+    int max = amount + 1;
+    vector<int> dp(max, max);
+    dp[0] = 0;
+
+    for (int i = 1; i <= amount; i++)
     {
-      long remainder = i;
-      int position = coins.size() - 1;
-      int count = 0;
-      while (remainder > 0 && position >= 0)
+      for (int j = 0; j < coins.size(); j++)
       {
-        if (coins[position] > remainder)
+        if (coins[j] <= i)
         {
-          position--;
-        }
-        else
-        {
-          remainder -= coins[position];
-          count++;
+          dp[i] = min(dp[i], dp[i - coins[j]] + 1);
         }
       }
-
-      remainders->push_back(remainder == 0 ? count : -1);
-    }
-    
-    int answer = 0;
-    int r = 0;
-    if (amount < coins.back())
-    {
-      answer = (*remainders)[amount];
-
-      delete remainders;
-
-      return answer;
     }
 
-    answer = amount / coins.back();
-    r = amount % coins.back();
-
-    if ((*remainders)[r] == -1)
-    {
-      return -1;
-    }
-
-    answer += (*remainders)[r];
-
-    delete remainders;
-
-    return answer;
+    return dp[amount] > amount ? -1 : dp[amount];
   }
 };
 
@@ -64,9 +31,10 @@ int main()
 {
   Solution solution;
 
-  // int result1 = solution.coinChange(*new vector<int>{1, 2, 5}, 11);
-  // int result2 = solution.coinChange(*new vector<int>{2, 5}, 11);
-  // int result3 = solution.coinChange(*new vector<int>{1}, 0);
-  // int result4 = solution.coinChange(*new vector<int>{1}, 1);
+  int result1 = solution.coinChange(*new vector<int>{1, 2, 5}, 11);
+  int result2 = solution.coinChange(*new vector<int>{2, 5}, 11);
+  int result3 = solution.coinChange(*new vector<int>{1}, 0);
+  int result4 = solution.coinChange(*new vector<int>{1}, 1);
   int result5 = solution.coinChange(*new vector<int>{2147483647}, 2);
+  int result6 = solution.coinChange(*new vector<int>{2}, 3);
 }
