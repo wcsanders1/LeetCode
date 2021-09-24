9145c5b5cd3cfe89ec6ce824dad5c778eb9d841b
William Sanders <william.sanders076@gmail.com>
2021-09-23 20:54:34 -0400

    CPP solution to MaximumProductsSubarray

--- MaximumProductSubarray/CPP/MaximumProductSubarray.cpp
+++ MaximumProductSubarray/CPP/MaximumProductSubarray.cpp
@@ -1,5 +1,6 @@
 #include <vector>
 #include <algorithm>
+#include <cmath>
 
 using namespace std;
 
@@ -13,39 +14,23 @@ public:
       return 0;
     }
 
-    if (nums.size() == 1)
-    {
-      return nums[0];
-    }
-
-    int negative = nums[0];
-    int positive = nums[0];
     int highest = nums[0];
+    int currentMax = nums[0];
+    int currentMin = nums[0];
+    int maximum = 0;
+    int minimum = 0;
 
     for (int i = 1; i < nums.size(); i++)
     {
       int n = nums[i];
-      if (n == 0)
-      {
-        highest = max(0, max(highest, max(negative, positive)));
-        negative = 0;
-        positive = 0;
-      }
-      else if (n > 0)
-      {
-        positive = positive == 0 ? n : positive * n;
-        negative = negative == 0 ? n : negative * n;
-      }
-      else
-      {
-        negative = negative == 0 ? n : negative * n;
-        positive = max(positive, negative);
-        highest = max(highest, positive);
-        positive = 0;
-      }
+      maximum = max(max(currentMax * n, currentMin * n), n);
+      minimum = min(min(currentMax * n, currentMin * n), n);
+      highest = max(maximum, highest);
+      currentMax = maximum;
+      currentMin = minimum;
     }
 
-    return max(highest, max(negative, positive));
+    return highest;
   }
 };
 
@@ -57,4 +42,5 @@ int main()
   int result3 = solution.maxProduct(*new vector<int>{7, -2, -4});        // 56
   int result4 = solution.maxProduct(*new vector<int>{6, 3, -10, 0, 2});  // 18
   int result5 = solution.maxProduct(*new vector<int>{2, -5, -2, -4, 3}); //24
+  int result6 = solution.maxProduct(*new vector<int>{3, -1, 4});         //4
 }
