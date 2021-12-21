87dabe5ed9e3f91817a72d24d37855be0065d716
William Sanders <william.sanders076@gmail.com>
2021-12-21 06:58:46 -0500

    PalindromePartitioning slow solution

---
+++ PalindromePartitioningII/CPP/PalindromePartitioningII.cpp
@@ -0,0 +1,57 @@
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Solution
+{
+public:
+  int minCut(string s)
+  {
+    if (isPalindrome(s))
+    {
+      return 0;
+    }
+
+    int result = INT32_MAX;
+    for (int i = 0; i < s.size() - 1; i++)
+    {
+      int tempResult = minCut(s.substr(0, i + 1)) + minCut(s.substr(i + 1, s.size() - i)) + 1;
+      if (tempResult < result)
+      {
+        result = tempResult;
+      }
+    }
+
+    return result;
+  }
+
+private:
+  bool isPalindrome(string s)
+  {
+    int start = 0;
+    int end = s.size() - 1;
+
+    while (start <= end)
+    {
+      if (s[start++] != s[end--])
+      {
+        return false;
+      }
+    }
+
+    return true;
+  }
+};
+
+int main()
+{
+  Solution solution;
+
+  int result1 = solution.minCut("baab");     // 0
+  int result2 = solution.minCut("aab");      // 1
+  int result3 = solution.minCut("a");        // 0
+  int result4 = solution.minCut("ab");       // 1
+  int result5 = solution.minCut("aabbccdd"); // 3
+  // int result6 = solution.minCut("eegiicgaeadbcfacfhifdbiehbgejcaeggcgbahfcajfhjjdgj");
+}
