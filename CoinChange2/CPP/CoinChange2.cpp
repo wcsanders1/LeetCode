45b0bfe779c6b2ac6e22e5aad6995c11bfd9eb0a
Bill Sanders <william.sanders.dev@gmail.com>
2022-01-17 14:36:57 -0500

    CoinChange2 CPP solution

--- CoinChange2/CPP/CoinChange2.cpp
+++ CoinChange2/CPP/CoinChange2.cpp
@@ -3,9 +3,29 @@
 
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    int change(int amount, vector<int>& coins) {
-        
+    int change(int amount, vector<int> &coins)
+    {
+        vector<int> dp(amount + 1, 0);
+        dp[0] = 1;
+        for (int coin : coins)
+        {
+            for (int i = coin; i <= amount; i++)
+            {
+                dp[i] += dp[i - coin];
+            }
+        }
+
+        return dp[amount];
     }
 };
+
+int main()
+{
+    Solution solution;
+    int result1 = solution.change(5, *new vector<int>{1, 2, 5});
+    int result2 = solution.change(3, *new vector<int>{2});
+    int result3 = solution.change(10, *new vector<int>{10});
+}
