ad9aebc515f383dc7a4038daa7b18bfb51991c6f
William Sanders <william.sanders076@gmail.com>
2022-01-27 07:04:18 -0500

    DeleteDuplicateFolders attempt

--- DeleteDuplicateFolders/CPP/DeleteDuplicateFolders.cpp
+++ DeleteDuplicateFolders/CPP/DeleteDuplicateFolders.cpp
@@ -9,6 +9,7 @@ class Node
 public:
   string Value;
   vector<Node *> Children;
+  bool canDelete;
 };
 
 class Solution
@@ -18,7 +19,6 @@ public:
   {
     Node *tree = new Node();
     tree->Value = "/";
-    map<string, Node *> childrenToParents;
 
     for (vector<string> path : paths)
     {
@@ -46,8 +46,37 @@ public:
       }
     }
 
+    map<string, vector<Node *> *> childrenToParents;
+
     vector<vector<string>> result;
     return result;
+  }
+
+private:
+  string mapChildrenToParents(Node &node, map<string, vector<Node *>> &childrenToParents)
+  {
+    for (Node *child : node.Children)
+    {
+      string descendants = mapChildrenToParents(*child, childrenToParents);
+      if (childrenToParents.count(descendants))
+      {
+        bool canDelete = true;
+        for (Node *subchild : child->Children)
+        {
+          if (!subchild->canDelete)
+          {
+            canDelete = false;
+            break;
+          }
+        }
+        child->canDelete = canDelete;
+        childrenToParents[descendants].push_back(child);
+      }
+      else
+      {
+        childrenToParents.insert({descendants, vector<Node *>{child}});
+      }
+    }
   }
 };
 
