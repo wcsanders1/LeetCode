b8a1a15effb1da3fb4afeeb35d225ed499366dbd
William Sanders <william.sanders076@gmail.com>
2022-03-02 06:02:40 -0500

    CountingBits CPP solution

--- CountingBits/CPP/CountingBits.cpp
+++ CountingBits/CPP/CountingBits.cpp
@@ -7,5 +7,39 @@ class Solution
 public:
   vector<int> countBits(int n)
   {
+    vector<int> answer(n + 1, 0);
+    int goBack = 1;
+    int lastPowerOfTwo = 0;
+    for (int i = 1; i <= n; i++)
+    {
+      if (isPowerOfTwo(i))
+      {
+        lastPowerOfTwo = i;
+        answer[i] = 1;
+        goBack = i / 2;
+      }
+      else if (i >= lastPowerOfTwo + goBack)
+      {
+        answer[i] = answer[i - goBack] + 1;
+      }
+      else
+      {
+        answer[i] = answer[i - goBack];
+      }
+    }
+    return answer;
+  }
+
+private:
+  bool isPowerOfTwo(int n)
+  {
+    return (n & (n - 1)) == 0;
   }
 };
+
+int main()
+{
+  Solution solution;
+
+  vector<int> result1 = solution.countBits(16);
+}
