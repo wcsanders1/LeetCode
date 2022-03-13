e30a589db88bf70a2992b256e30d5b1bd59c731e
Bill Sanders <william.sanders.dev@gmail.com>
2022-03-12 20:43:56 -0500

    LargestDivisibleSubset CPP solution

--- LargestDivisibleSubset/CPP/LargestDivisibleSubset.cpp
+++ LargestDivisibleSubset/CPP/LargestDivisibleSubset.cpp
@@ -11,36 +11,63 @@ public:
   {
     sort(nums.begin(), nums.end(), greater<>());
     unordered_map<int, vector<int>> graph;
-    vector<int> greatestSubset(0);
+    vector<int> greatestSubset(1, nums[0]);
 
     for (int i = 0; i < nums.size(); i++)
     {
-      if (graph.count(nums[i]) > 0)
+      populateGraph(graph, nums, i);
+    }
+
+    for (auto it : graph)
+    {
+      if (it.second.size() > greatestSubset.size())
+      {
+        greatestSubset = it.second;
+      }
+    }
+
+    return greatestSubset;
+  }
+
+private:
+  void populateGraph(unordered_map<int, vector<int>> &graph, vector<int> &nums, int position)
+  {
+    int num = nums[position];
+    vector<int> subset(1, num);
+    for (int i = position + 1; i < nums.size(); i++)
+    {
+      if (num % nums[i] != 0)
       {
         continue;
       }
 
-      graph[nums[i]] = vector<int>(0);
-
-      for (int j = i + 1; j < nums.size(); j++)
+      if (graph.count(nums[i]) == 0)
       {
-        if (graph.count(nums[j]) > 0)
-        {
-          continue;
-        }
+        populateGraph(graph, nums, i);
+      }
 
-        if (nums[i] % nums[j] != 0)
+      if (graph[nums[i]].size() + 1 > subset.size())
+      {
+        subset.clear();
+        subset.push_back(num);
+        for (int num : graph[nums[i]])
         {
-          continue;
+          subset.push_back(num);
         }
-
-        // vector<int> subset = getSubset();
       }
     }
-  }
 
-private:
-  vector<int> getSubset(unordered_map<int, vector<int>> &dp, vector<int> &nums, int position)
-  {
+    graph[num] = subset;
   }
 };
+
+int main()
+{
+  Solution solution;
+  vector<int> result1 = solution.largestDivisibleSubset(*new vector<int>{1, 2, 3});
+  vector<int> result2 = solution.largestDivisibleSubset(*new vector<int>{1, 2, 4, 8});
+  vector<int> result3 = solution.largestDivisibleSubset(*new vector<int>{1, 2, 5, 8, 10});
+  vector<int> result4 = solution.largestDivisibleSubset(*new vector<int>{1});
+  vector<int> result5 = solution.largestDivisibleSubset(*new vector<int>{3, 17});
+  vector<int> result6 = solution.largestDivisibleSubset(*new vector<int>{1, 2, 4, 8, 9, 72});
+}
