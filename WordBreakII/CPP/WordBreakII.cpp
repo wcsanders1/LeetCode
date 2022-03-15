4e517f674b4a444e285a5b11622c727ca1232c60
William Sanders <william.sanders076@gmail.com>
2022-03-15 06:06:24 -0400

    WordBreakII CPP solution

--- WordBreakII/CPP/WordBreakII.cpp
+++ WordBreakII/CPP/WordBreakII.cpp
@@ -1,6 +1,7 @@
 #include <vector>
 #include <string>
 #include <unordered_set>
+#include <unordered_map>
 
 using namespace std;
 
@@ -9,22 +10,66 @@ class Solution
 public:
   vector<string> wordBreak(string s, vector<string> &wordDict)
   {
-    vector<string> combinations;
     unordered_set<string> set(wordDict.begin(), wordDict.end());
-    for (int i = 0; i < s.size(); i++)
-    {
-      string words = getWords(set, s, i);
-      if (words.size() > 0)
-      {
-        combinations.push_back(words);
-      }
-    }
+    unordered_map<int, vector<string>> dp;
+    vector<string> combinations = getWords(set, s, 0, dp);
 
     return combinations;
   }
 
 private:
-  string getWords(unordered_set<string> &set, string s, int position)
+  vector<string> getWords(unordered_set<string> &set, string s, int position, unordered_map<int, vector<string>> &dp)
   {
+    vector<string> words;
+    for (int i = position + 1; i <= s.size(); i++)
+    {
+      string word = s.substr(position, i - position);
+      if (set.find(word) == set.end())
+      {
+        if (i == s.size())
+        {
+          return words;
+        }
+
+        continue;
+      }
+
+      if (i == s.size())
+      {
+        words.push_back(word);
+        return words;
+      }
+
+      if (dp.count(i) > 0)
+      {
+        for (string w : dp[i])
+        {
+          words.push_back(word + " " + w);
+        }
+
+        continue;
+      }
+
+      vector<string> newWords = getWords(set, s, i, dp);
+      if (newWords.size() > 0)
+      {
+        dp[i] = newWords;
+        for (string w : dp[i])
+        {
+          words.push_back(word + " " + w);
+        }
+      }
+    }
+
+    return words;
   }
 };
+
+int main()
+{
+  Solution solution;
+
+  vector<string> result1 = solution.wordBreak("catsanddog", vector<string>{"cat", "cats", "and", "sand", "dog"});
+  vector<string> result2 = solution.wordBreak("pineapplepenapple", vector<string>{"apple", "pen", "applepen", "pine", "pineapple"});
+  vector<string> result3 = solution.wordBreak("catsandog", vector<string>{"cats", "dog", "sand", "and", "cat"});
+}
