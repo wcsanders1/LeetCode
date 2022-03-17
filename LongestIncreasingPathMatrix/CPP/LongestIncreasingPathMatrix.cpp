5346a294d655bf1db598f1b88e8e7ee0ef77c917
William Sanders <william.sanders076@gmail.com>
2022-03-17 05:53:07 -0400

    LongestIncreasingPathMatrix CPP solution

--- LongestIncreasingPathMatrix/CPP/LongestIncreasingPathMatrix.cpp
+++ LongestIncreasingPathMatrix/CPP/LongestIncreasingPathMatrix.cpp
@@ -9,11 +9,16 @@ public:
   int longestIncreasingPath(vector<vector<int>> &matrix)
   {
     int answer = 0;
-    vector<vector<int>> dp(matrix.size(), vector<int>(matrix.size(), -1));
+    if (matrix.size() == 0)
+    {
+      return answer;
+    }
+
+    vector<vector<int>> dp(matrix.size(), vector<int>(matrix[0].size(), -1));
 
     for (int row = 0; row < matrix.size(); row++)
     {
-      for (int column = 0; column < matrix.size(); column++)
+      for (int column = 0; column < matrix[row].size(); column++)
       {
         if (dp[row][column] == -1)
         {
@@ -30,7 +35,7 @@ public:
 private:
   void getGreatestIncrease(vector<vector<int>> &matrix, vector<vector<int>> &dp, int row, int column)
   {
-    int greatestIncrease = 0;
+    int greatestIncrease = 1;
     int num = matrix[row][column];
     if (column > 0 && matrix[row][column - 1] > num)
     {
@@ -41,7 +46,7 @@ private:
       greatestIncrease = max(greatestIncrease, dp[row][column - 1] + 1);
     }
 
-    if (column < matrix.size() - 1 && matrix[row][column + 1] > num)
+    if (column < matrix[row].size() - 1 && matrix[row][column + 1] > num)
     {
       if (dp[row][column + 1] == -1)
       {
@@ -54,7 +59,7 @@ private:
     {
       if (dp[row - 1][column] == -1)
       {
-        getGreatestIncrease(matrix, dp, column, row - 1);
+        getGreatestIncrease(matrix, dp, row - 1, column);
       }
       greatestIncrease = max(greatestIncrease, dp[row - 1][column] + 1);
     }
@@ -63,7 +68,7 @@ private:
     {
       if (dp[row + 1][column] == -1)
       {
-        getGreatestIncrease(matrix, dp, column, row + 1);
+        getGreatestIncrease(matrix, dp, row + 1, column);
       }
       greatestIncrease = max(greatestIncrease, dp[row + 1][column] + 1);
     }
@@ -76,4 +81,6 @@ int main()
   Solution solution;
 
   int result1 = solution.longestIncreasingPath(vector<vector<int>>{vector<int>{9, 9, 4}, vector<int>{6, 6, 8}, vector<int>{2, 1, 1}});
+  int result2 = solution.longestIncreasingPath(vector<vector<int>>{vector<int>{3, 4, 5}, vector<int>{3, 2, 6}, vector<int>{2, 2, 1}});
+  int result3 = solution.longestIncreasingPath(vector<vector<int>>{vector<int>{1, 2}});
 }
