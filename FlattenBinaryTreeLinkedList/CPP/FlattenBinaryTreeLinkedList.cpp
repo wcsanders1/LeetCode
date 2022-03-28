6077981ffbe729d8d06f6d548c6145d6d51dbc5e
William Sanders <william.sanders076@gmail.com>
2022-03-28 06:05:29 -0400

    FlattenBinaryTreeLinkedList CPP solution

--- FlattenBinaryTreeLinkedList/CPP/FlattenBinaryTreeLinkedList.cpp
+++ FlattenBinaryTreeLinkedList/CPP/FlattenBinaryTreeLinkedList.cpp
@@ -16,5 +16,49 @@ class Solution
 public:
   void flatten(TreeNode *root)
   {
+    if (root == nullptr)
+    {
+      return;
+    }
+
+    TreeNode preorder = TreeNode(root->val);
+    makePreorder(root, &preorder);
+
+    *root = preorder;
+  }
+
+private:
+  void makePreorder(TreeNode *root, TreeNode *preOrder)
+  {
+    if (root == nullptr)
+    {
+      return;
+    }
+
+    TreeNode *temp = preOrder;
+    if (root->left != nullptr)
+    {
+      temp->right = new TreeNode(root->left->val);
+      makePreorder(root->left, temp->right);
+    }
+
+    while (temp->right != nullptr)
+    {
+      temp = temp->right;
+    }
+
+    if (root->right != nullptr)
+    {
+      temp->right = new TreeNode(root->right->val);
+      makePreorder(root->right, temp->right);
+    }
   }
 };
+
+int main()
+{
+  Solution solution;
+
+  TreeNode *node1 = new TreeNode(1, new TreeNode(2, new TreeNode(3), new TreeNode(4)), new TreeNode(5, nullptr, new TreeNode(6)));
+  solution.flatten(node1);
+}
