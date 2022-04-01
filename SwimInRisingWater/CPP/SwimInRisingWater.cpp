4c22a7da45ce74540971802f7eb71cc00ea79fe2
William Sanders <william.sanders076@gmail.com>
2022-04-01 06:02:57 -0400

    SwimInRisingWater CPP wip

--- SwimInRisingWater/CPP/SwimInRisingWater.cpp
+++ SwimInRisingWater/CPP/SwimInRisingWater.cpp
@@ -1,7 +1,5 @@
 // https://leetcode.com/problems/swim-in-rising-water/
 #include <vector>
-#include <unordered_map>
-#include <queue>
 
 using namespace std;
 
@@ -10,52 +8,43 @@ class Solution
 public:
   int swimInWater(vector<vector<int>> &grid)
   {
-    unordered_map<int, vector<int>> graph;
-    vector<int> distances(grid.size() * grid[0].size(), INT_MAX);
-    distances[0] = 0;
+    vector<vector<int>> distances(grid.size(), vector<int>(grid[0].size(), INT_MAX));
+    distances[0][0] = 0;
 
     for (int row = 0; row < grid.size(); row++)
     {
       for (int col = 0; col < grid[row].size(); col++)
       {
-        int num = grid[row][col];
-        graph[num] = vector<int>(0);
+        if (row == 0 && col == 0)
+        {
+          continue;
+        }
+
+        int cost = grid[row][col];
 
         if (row > 0)
         {
-          int nextNum = grid[row - 1][col];
-          graph[num].push_back(nextNum);
+          int sourceCost = grid[row - 1][col];
+          distances[row][col] = min(distances[row][col], cost + sourceCost);
         }
 
         if (row < grid.size() - 1)
         {
-          int nextNum = grid[row + 1][col];
-          graph[num].push_back(nextNum);
+          int sourceCost = grid[row + 1][col];
+          distances[row][col] = min(distances[row][col], cost + sourceCost);
         }
 
         if (col > 0)
         {
-          int nextNum = grid[row][col - 1];
-          graph[num].push_back(nextNum);
+          int sourceCost = grid[row][col - 1];
+          distances[row][col] = min(distances[row][col], cost + sourceCost);
         }
 
         if (col < grid[row].size() - 1)
         {
-          int nextNum = grid[row][col + 1];
-          graph[num].push_back(nextNum);
+          int sourceCost = grid[row][col + 1];
+          distances[row][col] = min(distances[row][col], cost + sourceCost);
         }
-      }
-    }
-
-    queue<pair<int, vector<int>>> q;
-    q.emplace(pair<int, vector<int>>(0, graph[0]));
-    while (!q.empty())
-    {
-      auto const &[source, nodes] = q.front();
-      q.pop();
-
-      for (int node : nodes)
-      {
       }
     }
   }
