481fc9992e798fd5739274d69a3388e3ad2f7540
William Sanders <william.sanders076@gmail.com>
2022-04-05 06:02:06 -0400

    SwimInRisingWater CPP solution

--- SwimInRisingWater/CPP/SwimInRisingWater.cpp
+++ SwimInRisingWater/CPP/SwimInRisingWater.cpp
@@ -1,5 +1,6 @@
 // https://leetcode.com/problems/swim-in-rising-water/
 #include <vector>
+#include <queue>
 
 using namespace std;
 
@@ -9,43 +10,82 @@ public:
   int swimInWater(vector<vector<int>> &grid)
   {
     vector<vector<int>> distances(grid.size(), vector<int>(grid[0].size(), INT_MAX));
-    distances[0][0] = 0;
+    distances[0][0] = grid[0][0];
 
-    for (int row = 0; row < grid.size(); row++)
+    queue<vector<int>> q;
+    q.emplace(vector<int>{0, 0});
+    while (!q.empty())
     {
-      for (int col = 0; col < grid[row].size(); col++)
+      vector<int> node = q.front();
+      q.pop();
+
+      int row = node[0];
+      int col = node[1];
+      int parentDistance = distances[row][col];
+
+      if (row > 0)
       {
-        if (row == 0 && col == 0)
+        int childDistance = distances[row - 1][col];
+        int childValue = grid[row - 1][col];
+        if (childDistance > childValue && childDistance > parentDistance)
         {
-          continue;
+          distances[row - 1][col] = max(childValue, parentDistance);
+          q.emplace(vector<int>{row - 1, col});
         }
+      }
 
-        int cost = grid[row][col];
-
-        if (row > 0)
+      if (row < grid.size() - 1)
+      {
+        int childDistance = distances[row + 1][col];
+        int childValue = grid[row + 1][col];
+        if (childDistance > childValue && childDistance > parentDistance)
         {
-          int sourceCost = grid[row - 1][col];
-          distances[row][col] = min(distances[row][col], cost + sourceCost);
+          distances[row + 1][col] = max(childValue, parentDistance);
+          q.emplace(vector<int>{row + 1, col});
         }
+      }
 
-        if (row < grid.size() - 1)
+      if (col > 0)
+      {
+        int childDistance = distances[row][col - 1];
+        int childValue = grid[row][col - 1];
+        if (childDistance > childValue && childDistance > parentDistance)
         {
-          int sourceCost = grid[row + 1][col];
-          distances[row][col] = min(distances[row][col], cost + sourceCost);
+          distances[row][col - 1] = max(childValue, parentDistance);
+          q.emplace(vector<int>{row, col - 1});
         }
+      }
 
-        if (col > 0)
+      if (col < grid[row].size() - 1)
+      {
+        int childDistance = distances[row][col + 1];
+        int childValue = grid[row][col + 1];
+        if (childDistance > childValue && childDistance > parentDistance)
         {
-          int sourceCost = grid[row][col - 1];
-          distances[row][col] = min(distances[row][col], cost + sourceCost);
-        }
-
-        if (col < grid[row].size() - 1)
-        {
-          int sourceCost = grid[row][col + 1];
-          distances[row][col] = min(distances[row][col], cost + sourceCost);
+          distances[row][col + 1] = max(childValue, parentDistance);
+          q.emplace(vector<int>{row, col + 1});
         }
       }
     }
+
+    return distances[grid.size() - 1][grid[0].size() - 1];
   }
 };
+
+int main()
+{
+  Solution solution;
+
+  int result1 = solution.swimInWater(vector<vector<int>>{
+      vector<int>{0, 1, 2, 3, 4},
+      vector<int>{24, 23, 22, 21, 5},
+      vector<int>{12, 13, 14, 15, 16},
+      vector<int>{11, 17, 18, 19, 20},
+      vector<int>{10, 9, 8, 7, 6}});
+
+  int result2 = solution.swimInWater(vector<vector<int>>{
+      vector<int>{0, 2}, vector<int>{1, 3}});
+
+  int result3 = solution.swimInWater(vector<vector<int>>{
+      vector<int>{3, 2}, vector<int>{0, 1}});
+}
