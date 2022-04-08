a930d383fb8e675fd793e981202189c0e0e5bb16
William Sanders <william.sanders076@gmail.com>
2022-04-08 06:03:42 -0400

    PathWithMinimumEffort wip

--- PathWithMinimumEffort/CPP/PathWithMinimumEffort.cpp
+++ PathWithMinimumEffort/CPP/PathWithMinimumEffort.cpp
@@ -1,5 +1,6 @@
 // https://leetcode.com/problems/path-with-minimum-effort/
 #include <vector>
+#include <queue>
 
 using namespace std;
 
@@ -8,5 +9,23 @@ class Solution
 public:
   int minimumEffortPath(vector<vector<int>> &heights)
   {
+    vector<vector<int>> efforts(heights.size(), vector<int>(heights[0].size(), INT_MAX));
+    efforts[0][0] = heights[0][0];
+    vector<vector<int>> directions{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+
+    queue<vector<int>> q;
+    q.emplace(vector<int>{0, 0});
+    while (!q.empty())
+    {
+      vector<int> node = q.front();
+      q.pop();
+
+      int height = heights[node[0]][node[1]];
+      for (vector<int> direction : directions)
+      {
+      }
+    }
+
+    return efforts[efforts.size() - 1][efforts[0].size() - 1];
   }
 };
