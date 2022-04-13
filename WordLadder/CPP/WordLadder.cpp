4a8d477f9ddf5200bcd5ddaf19f9d2d57f44904e
William Sanders <william.sanders076@gmail.com>
2022-04-13 06:02:28 -0400

    WordLadder wip

--- WordLadder/CPP/WordLadder.cpp
+++ WordLadder/CPP/WordLadder.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <queue>
 #include <unordered_map>
+#include <unordered_set>
 
 using namespace std;
 
@@ -11,8 +12,11 @@ public:
   int ladderLength(string beginWord, string endWord, vector<string> &wordList)
   {
     unordered_map<string, vector<string>> graph;
+    wordList.push_back(beginWord);
     for (string word : wordList)
     {
+      graph[word] = vector<string>(0);
+
       for (auto const &kv : graph)
       {
         string key = kv.first;
@@ -28,18 +32,52 @@ public:
         if (differences == 1)
         {
           graph[key].push_back(word);
+          graph[word].push_back(key);
         }
       }
+    }
 
-      if (graph.count(word) == 0)
+    int answer = 0;
+    queue<string> q;
+    unordered_set<string> visited;
+    q.emplace(beginWord);
+    visited.insert(beginWord);
+
+    while (!q.empty())
+    {
+      answer++;
+      string word = q.front();
+      q.pop();
+
+      vector<string> connections = graph[word];
+      if (connections.size() == 0)
       {
-        graph[word] = vector<string>(0);
+        return 0;
+      }
+
+      for (string connection : connections)
+      {
+        if (connection == endWord)
+        {
+          return answer;
+        }
+
+        if (visited.find(connection) == visited.end())
+        {
+          visited.insert(connection);
+          q.emplace(connection);
+        }
       }
     }
 
-    for (auto const &kv : graph)
-    {
-      string key = kv.first;
-    }
+    return 0;
   }
 };
+
+int main()
+{
+  Solution solution;
+
+  int result1 = solution.ladderLength("hit", "cog", vector<string>{"hot", "dot", "dog", "lot", "log", "cog"});
+  int result2 = solution.ladderLength("hit", "cog", vector<string>{"hot", "dot", "dog", "lot", "log"});
+}
