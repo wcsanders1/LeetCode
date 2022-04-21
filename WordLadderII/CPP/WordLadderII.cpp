54668de728107882123a2a099433f54007652a61
William Sanders <william.sanders076@gmail.com>
2022-04-21 05:57:44 -0400

    WordLadderII CPP solution

--- WordLadderII/CPP/WordLadderII.cpp
+++ WordLadderII/CPP/WordLadderII.cpp
@@ -8,124 +8,56 @@ using namespace std;
 
 class Solution
 {
+  // not mine
 public:
   vector<vector<string>> findLadders(string beginWord, string endWord, vector<string> &wordList)
   {
-    unordered_map<string, vector<string>> *graph = getGraph(beginWord, endWord, wordList);
-    int shortestLength = getShortestLength(beginWord, endWord, *graph);
-    if (shortestLength == 0)
-    {
-      return vector<vector<string>>(0);
-    }
-
+    unordered_set<string> wordSet(wordList.begin(), wordList.end());
     unordered_set<string> visited;
-    vector<vector<string>> paths;
-    getPaths(beginWord, endWord, *graph, visited, paths, shortestLength);
-
-    return paths;
-  }
-
-private:
-  bool getPaths(string beginWord, string endWord, unordered_map<string, vector<string>> &graph, unordered_set<string> &visited, vector<vector<string>> &paths, int maxLevel, int level = 0)
-  {
-    if (level == maxLevel)
-    {
-      if (beginWord == endWord)
-      {
-        paths.push_back(vector<string>{endWord});
-        return true;
-      }
-      return false;
-    }
-
-    bool pathExists = false;
-    for (string connection : graph[beginWord])
-    {
-      if (visited.find(connection) == visited.end())
-      {
-        visited.insert(connection);
-        if (getPaths(connection, endWord, graph, visited, paths, maxLevel, level + 1))
-        {
-          pathExists = true;
-          for (vector<string> &path : paths)
-          {
-            path.insert(path.begin(), beginWord);
-          }
-        }
-
-        visited.erase(connection);
-      }
-    }
-
-    return pathExists;
-  }
-
-  unordered_map<string, vector<string>> *getGraph(string beginWord, string endWord, vector<string> &wordList)
-  {
-    unordered_map<string, vector<string>> *graph = new unordered_map<string, vector<string>>();
-    wordList.push_back(beginWord);
-    for (string word : wordList)
-    {
-      (*graph)[word] = vector<string>(0);
-
-      for (auto const &kv : *graph)
-      {
-        string key = kv.first;
-        int differences = 0;
-        for (int i = 0; i < key.length(); i++)
-        {
-          if (key[i] != word[i])
-          {
-            differences++;
-          }
-        }
-
-        if (differences == 1)
-        {
-          (*graph)[key].push_back(word);
-          (*graph)[word].push_back(key);
-        }
-      }
-    }
-
-    return graph;
-  }
-
-  int getShortestLength(string beginWord, string endWord, unordered_map<string, vector<string>> &graph)
-  {
-    int answer = 1;
-    queue<string> q;
-    unordered_set<string> visited;
-    q.emplace(beginWord);
-    visited.insert(beginWord);
+    vector<vector<string>> answer;
+    queue<vector<string>> q;
+    q.push({beginWord});
 
     while (!q.empty())
     {
       int qSize = q.size();
-      for (int i = 0; i < qSize; i++)
+      while (qSize-- > 0)
       {
-        string word = q.front();
+        vector<string> currentPath = q.front();
         q.pop();
-
-        if (word == endWord)
+        string lastWord = currentPath.back();
+        for (int i = 0; i < lastWord.size(); i++)
         {
-          return answer;
-        }
-
-        for (string connection : graph[word])
-        {
-          if (visited.find(connection) == visited.end())
+          string temp = lastWord;
+          for (char c = 'a'; c <= 'z'; c++)
           {
-            visited.insert(connection);
-            q.emplace(connection);
+            temp[i] = c;
+            if (wordSet.find(temp) != wordSet.end())
+            {
+              vector<string> newPath = currentPath;
+              newPath.push_back(temp);
+              visited.insert(temp);
+              if (temp == endWord)
+              {
+                answer.push_back(newPath);
+              }
+              else
+              {
+                q.push(newPath);
+              }
+            }
           }
         }
       }
 
-      answer++;
+      for (const string &s : visited)
+      {
+        wordSet.erase(s);
+      }
+      visited.clear();
     }
 
-    return 0;
+    return answer;
   }
 };
 
