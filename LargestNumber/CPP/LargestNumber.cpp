380a086e1dbba9df373fca4448dda8beea91deb8
William Sanders <william.sanders076@gmail.com>
2022-05-09 06:39:10 -0400

    LargestNumber CPP wip

--- LargestNumber/CPP/LargestNumber.cpp
+++ LargestNumber/CPP/LargestNumber.cpp
@@ -8,8 +8,8 @@ using namespace std;
 class Trie
 {
 public:
-  bool isValue = false;
-  unordered_map<string, Trie> children;
+  int amount = 0;
+  unordered_map<int, Trie *> children;
 };
 
 class Solution
@@ -20,42 +20,80 @@ public:
     Trie root;
     for (int num : nums)
     {
-      vector<string> *digits = getDigits(num);
-      Trie tempNode = root;
+      vector<int> *digits = getDigits(num);
+      Trie *tempNode = &root;
       for (int i = digits->size() - 1; i >= 0; i--)
       {
-        string digit = (*digits)[i];
-        if (tempNode.children.find(digit) == tempNode.children.end())
+        int digit = (*digits)[i];
+        if (tempNode->children.find(digit) == tempNode->children.end())
         {
-          Trie newNode;
-          tempNode.children[digit] = newNode;
+          Trie *newNode = new Trie;
+          tempNode->children[digit] = newNode;
           tempNode = newNode;
         }
         else
         {
-          tempNode = tempNode.children[digit];
+          tempNode = tempNode->children[digit];
         }
 
-        if (i == 0)
+        tempNode->amount++;
+      }
+    }
+
+    string answer = "";
+    for (int i = 9; i >= 0; i--)
+    {
+      if (root.children.find(i) != root.children.end())
+      {
+        while (root.children[i]->amount > 0)
         {
-          tempNode.isValue = true;
+          root.children[i]->amount--;
+          answer += to_string(i);
+          buildLongestNumber(answer, root.children[i]);
+        }
+      }
+    }
+
+    return answer;
+  }
+
+private:
+  vector<int> *getDigits(int num)
+  {
+    vector<int> *digits = new vector<int>(0);
+    while (num >= 10)
+    {
+      int rem = num % 10;
+      digits->push_back(rem);
+      num /= 10;
+    }
+
+    digits->push_back(num);
+    return digits;
+  }
+
+  void buildLongestNumber(string &longest, Trie *node)
+  {
+    for (int i = 9; i >= 0; i--)
+    {
+      if (node->children.find(i) != node->children.end())
+      {
+        if (node->children[i]->amount > 0)
+        {
+          node->children[i]->amount--;
+          longest += to_string(i);
+          buildLongestNumber(longest, node->children[i]);
+          break;
         }
       }
     }
   }
-
-private:
-  vector<string> *getDigits(int num)
-  {
-    vector<string> *digits = new vector<string>(0);
-    while (num > 10)
-    {
-      int rem = num % 10;
-      digits->push_back(to_string(rem));
-      num /= 10;
-    }
-
-    digits->push_back(to_string(num));
-    return digits;
-  }
 };
+
+int main()
+{
+  Solution solution;
+
+  // string result1 = solution.largestNumber(vector<int>{10, 2});
+  string result2 = solution.largestNumber(vector<int>{3, 30, 34, 5, 9});
+}
