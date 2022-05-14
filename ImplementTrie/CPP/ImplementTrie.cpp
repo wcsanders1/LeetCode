1ef87557dfea6b6f89647a07664ac39c01584764
William Sanders <william.sanders076@gmail.com>
2022-05-14 11:49:01 -0400

    ImplementTrie CPP scaffold

---
+++ ImplementTrie/CPP/ImplementTrie.cpp
@@ -0,0 +1,31 @@
+// https://leetcode.com/problems/implement-trie-prefix-tree/
+#include <string>
+
+using namespace std;
+class Trie
+{
+public:
+  Trie()
+  {
+  }
+
+  void insert(string word)
+  {
+  }
+
+  bool search(string word)
+  {
+  }
+
+  bool startsWith(string prefix)
+  {
+  }
+};
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie* obj = new Trie();
+ * obj->insert(word);
+ * bool param_2 = obj->search(word);
+ * bool param_3 = obj->startsWith(prefix);
+ */
