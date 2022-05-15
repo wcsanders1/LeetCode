8260e4aff049d8faa669586c782e2c4b6cb64696
Bill Sanders <william.sanders.dev@gmail.com>
2022-05-15 14:46:34 -0400

    WordSearchII CPP scaffold

--- WordSearchII/CPP/WordSearchII.cpp
+++ WordSearchII/CPP/WordSearchII.cpp
@@ -1,6 +1,7 @@
 // https://leetcode.com/problems/word-search-ii/
 #include <string>
 #include <vector>
+#include <unordered_map>
 
 using namespace std;
 
@@ -17,5 +18,13 @@ class Solution
 public:
     vector<string> findWords(vector<vector<char>> &board, vector<string> &words)
     {
+    }
+
+private:
+    unordered_map<char, vector<Point>> *makeGraph(vector<vector<char>> &board)
+    {
+        auto *graph = new unordered_map<char, vector<Point>>();
+
+        return graph;
     }
 };
