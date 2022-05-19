e64e1c308131c7c731b8e98f685d417049ba3e1d
William Sanders <william.sanders076@gmail.com>
2022-05-19 06:42:38 -0400

    PopulatingNextRightPointers.CPP better solution

--- PopulatingNextRightPointers/CPP/PopulatingNextRightPointers.cpp
+++ PopulatingNextRightPointers/CPP/PopulatingNextRightPointers.cpp
@@ -73,3 +73,21 @@ public:
     return root;
   }
 };
+
+// above is a bad solution because it takes O(N) memory; this takes N(1):
+// void connect(TreeLinkNode *root) {
+//     if(!root)
+//         return;
+//     while(root -> left)
+//     {
+//         TreeLinkNode *p = root;
+//         while(p)
+//         {
+//             p -> left -> next = p -> right;
+//             if(p -> next)
+//                 p -> right -> next = p -> next -> left;
+//             p = p -> next;
+//         }
+//         root = root -> left;
+//     }
+// }
