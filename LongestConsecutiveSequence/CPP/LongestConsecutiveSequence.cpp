33cdb430933541fbbf6ed3ac707d0e9e6ebd57a2
William Sanders <william.sanders076@gmail.com>
2022-05-19 06:50:38 -0400

    LongestConsecutiveSequence CPP scaffold

---
+++ LongestConsecutiveSequence/CPP/LongestConsecutiveSequence.cpp
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/longest-consecutive-sequence/
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+  int longestConsecutive(vector<int> &nums)
+  {
+  }
+};
