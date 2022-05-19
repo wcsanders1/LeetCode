2f52e16f4ea5df9d94d3eb80dd3f76dfdc4cf5b7
William Sanders <william.sanders076@gmail.com>
2022-05-19 05:47:27 -0400

    PopulatingNextRightPointers CPP solution

--- PopulatingNextRightPointers/CPP/PopulatingNextRightPointers.cpp
+++ PopulatingNextRightPointers/CPP/PopulatingNextRightPointers.cpp
@@ -1,5 +1,6 @@
 // https://leetcode.com/problems/populating-next-right-pointers-in-each-node/
 #include <vector>
+#include <queue>
 
 using namespace std;
 
@@ -24,5 +25,51 @@ class Solution
 public:
   Node *connect(Node *root)
   {
+    if (root == NULL)
+    {
+      return root;
+    }
+
+    queue<Node *> q;
+    Node *current = nullptr;
+    q.emplace(root);
+
+    while (!q.empty())
+    {
+      current = q.front();
+      q.pop();
+
+      int count = q.size();
+      if (current->left != NULL)
+      {
+        q.emplace(current->left);
+      }
+
+      if (current->right != NULL)
+      {
+        q.emplace(current->right);
+      }
+
+      for (int i = 0; i < count; i++)
+      {
+        Node *n = q.front();
+        q.pop();
+
+        current->next = n;
+        current = n;
+
+        if (current->left != NULL)
+        {
+          q.emplace(current->left);
+        }
+
+        if (current->right != NULL)
+        {
+          q.emplace(current->right);
+        }
+      }
+    }
+
+    return root;
   }
 };
