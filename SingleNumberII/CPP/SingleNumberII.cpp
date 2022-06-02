f0bb7615e5bac97a8aac691fb851547d9a018ef8
William Sanders <william.sanders076@gmail.com>
2022-06-02 05:37:01 -0400

    SingleNumberII CPP solution

--- SingleNumberII/CPP/SingleNumberII.cpp
+++ SingleNumberII/CPP/SingleNumberII.cpp
@@ -1,4 +1,6 @@
 // https://leetcode.com/problems/single-number-ii/
+// not mine
+// good explanation: https://leetcode.com/problems/single-number-ii/discuss/43368/constant-space-solution
 #include <vector>
 
 using namespace std;
@@ -8,5 +10,26 @@ class Solution
 public:
   int singleNumber(vector<int> &nums)
   {
+    int one = 0, two = 0, three = 0;
+    for (int i : nums)
+    {
+      two |= one & i;
+      one ^= i;
+      three = one & two;
+      one ^= three;
+      two ^= three;
+    }
+    return one;
   }
 };
+
+int main()
+{
+  Solution solution;
+
+  int result1 = solution.singleNumber(*new vector<int>{2, 2, 3, 2});
+  int result2 = solution.singleNumber(*new vector<int>{0, 1, 0, 1, 0, 1, 99});
+  int result3 = solution.singleNumber(*new vector<int>{1, 2, 3, 1, 2, 5, 3, 1, 2, 3});
+  int result4 = solution.singleNumber(*new vector<int>{1, 2, 1, 2, 4, 1, 2});
+  int result5 = solution.singleNumber(*new vector<int>{1, 2, 1, 2, 0, 1, 2});
+}
