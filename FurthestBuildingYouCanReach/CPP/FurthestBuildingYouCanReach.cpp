f68b3a578768761a8b4fc1617801fe148e49836d
William Sanders <william.sanders076@gmail.com>
2022-06-08 06:39:10 -0400

    FurthestBuildingYouCanReach CPP init

---
+++ FurthestBuildingYouCanReach/CPP/FurthestBuildingYouCanReach.cpp
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/furthest-building-you-can-reach/
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+  int furthestBuilding(vector<int> &heights, int bricks, int ladders)
+  {
+  }
+};
