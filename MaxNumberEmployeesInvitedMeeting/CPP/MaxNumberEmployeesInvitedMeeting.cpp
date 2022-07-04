3c4f1e692f6ad26ded55bf8b36bf59cc6ab97322
William Sanders <william.sanders076@gmail.com>
2022-07-04 15:53:38 -0400

    MaxNumberEmployeesInvitedMeeting CPP solution

--- MaxNumberEmployeesInvitedMeeting/CPP/MaxNumberEmployeesInvitedMeeting.cpp
+++ MaxNumberEmployeesInvitedMeeting/CPP/MaxNumberEmployeesInvitedMeeting.cpp
@@ -1,6 +1,7 @@
 // https://leetcode.com/problems/maximum-employees-to-be-invited-to-a-meeting/
+// not mine: https://leetcode.com/problems/maximum-employees-to-be-invited-to-a-meeting/discuss/1674049/Mutual-Favorites
 #include <vector>
-#include <unordered_map>
+#include <numeric>
 
 using namespace std;
 
@@ -9,17 +10,63 @@ class Solution
 public:
   int maximumInvitations(vector<int> &favorite)
   {
-    unordered_map<int, vector<int>> graph;
+    vector<vector<int>> reverseGraph(favorite.size());
     for (int i = 0; i < favorite.size(); i++)
     {
-      if (graph.find(favorite[i]) == graph.end())
+      if (favorite[favorite[i]] != i)
       {
-        graph[favorite[i]] = vector<int>();
+        reverseGraph[favorite[i]].push_back(i);
+      }
+    }
+
+    int result = 0;
+    vector<bool> visited(favorite.size(), false);
+    for (int i = 0; i < favorite.size(); i++)
+    {
+      if (favorite[favorite[i]] == i)
+      {
+        result += dfs(i, reverseGraph, visited);
+      }
+    }
+
+    for (int i = 0; i < favorite.size(); i++)
+    {
+      int count = 0;
+      int j = i;
+
+      while (!visited[j])
+      {
+        visited[j] = true;
+        j = favorite[j];
+        count++;
       }
 
-      graph[favorite[i]].push_back(i);
+      for (int k = i; k != j; k = favorite[k])
+      {
+        count--;
+      }
+
+      result = max(result, count);
     }
 
-    vector<int> maxCache(favorite.size(), 0);
-    }
+    return result;
+  }
+
+private:
+  int dfs(int index, vector<vector<int>> &reverseGraph, vector<bool> &visited)
+  {
+    visited[index] = true;
+    return 1 + accumulate(begin(reverseGraph[index]), end(reverseGraph[index]), 0, [&](int res, int j)
+                          { return max(res, dfs(j, reverseGraph, visited)); });
+  }
 };
+
+int main()
+{
+  Solution solution;
+
+  int result1 = solution.maximumInvitations(*new vector<int>{2, 2, 1, 2});                           // 3
+  int result2 = solution.maximumInvitations(*new vector<int>{1, 2, 0});                              // 3
+  int result3 = solution.maximumInvitations(*new vector<int>{3, 0, 1, 4, 1});                        // 4
+  int result4 = solution.maximumInvitations(*new vector<int>{1, 2, 3, 4, 5, 6, 3, 8, 9, 10, 11, 8}); // 4
+}
