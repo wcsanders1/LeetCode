0addf128cac0bc81646414cc1cb4b85aea73683f
William Sanders <william.sanders076@gmail.com>
2022-07-11 06:56:28 -0400

    Convert1DArrayInto2DArray CPP wip

---
+++ Convert1DArrayInto2DArray/CPP/Convert1DArrayInto2DArray.cpp
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/convert-1d-array-into-2d-array/
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+  vector<vector<int>> construct2DArray(vector<int> &original, int m, int n)
+  {
+  }
+};
