b421859c52466de8ef5c3fd84a39a572868f3edc
William Sanders <william.sanders076@gmail.com>
2022-07-12 05:52:55 -0400

    DesignCircularQueue CPP wip

---
+++ DesignCircularQueue/CPP/DesignCircularQueue.cpp
@@ -0,0 +1,36 @@
+// https://leetcode.com/problems/design-circular-queue/
+#include <vector>
+
+using namespace std;
+
+class MyCircularQueue
+{
+public:
+  MyCircularQueue(int k)
+  {
+  }
+
+  bool enQueue(int value)
+  {
+  }
+
+  bool deQueue()
+  {
+  }
+
+  int Front()
+  {
+  }
+
+  int Rear()
+  {
+  }
+
+  bool isEmpty()
+  {
+  }
+
+  bool isFull()
+  {
+  }
+};
