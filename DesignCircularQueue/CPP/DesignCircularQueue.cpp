f2cb1e186e10b921f7c6effe7ed0491b4ad408fe
William Sanders <william.sanders076@gmail.com>
2022-07-12 05:59:51 -0400

    DesignCircularQueue CPP wip

--- DesignCircularQueue/CPP/DesignCircularQueue.cpp
+++ DesignCircularQueue/CPP/DesignCircularQueue.cpp
@@ -8,6 +8,10 @@ class MyCircularQueue
 public:
   MyCircularQueue(int k)
   {
+    _capacity = k;
+    _size = 0;
+    _position = 0;
+    _queue = vector<int>(k, -1);
   }
 
   bool enQueue(int value)
@@ -20,17 +24,33 @@ public:
 
   int Front()
   {
+    if (_size == 0)
+    {
+      return -1;
+    }
   }
 
   int Rear()
   {
+    if (_size == 0)
+    {
+      return -1;
+    }
   }
 
   bool isEmpty()
   {
+    return _size == 0;
   }
 
   bool isFull()
   {
+    return _size == _capacity;
   }
+
+private:
+  int _capacity;
+  int _size;
+  int _position;
+  vector<int> _queue;
 };
