267db1d6a75f308861662467657fe5ca0bbb9557
William Sanders <william.sanders076@gmail.com>
2022-07-13 05:59:19 -0400

    DesignCircularQueue CPP solution

--- DesignCircularQueue/CPP/DesignCircularQueue.cpp
+++ DesignCircularQueue/CPP/DesignCircularQueue.cpp
@@ -10,16 +10,52 @@ public:
   {
     _capacity = k;
     _size = 0;
-    _position = 0;
+    _rearPosition = -1;
+    _frontPosition = 0;
     _queue = vector<int>(k, -1);
   }
 
   bool enQueue(int value)
   {
+    if (_size == _capacity)
+    {
+      return false;
+    }
+
+    if (_rearPosition == _capacity - 1)
+    {
+      _rearPosition = 0;
+    }
+    else
+    {
+      _rearPosition++;
+    }
+
+    _size++;
+    _queue[_rearPosition] = value;
+
+    return true;
   }
 
   bool deQueue()
   {
+    if (_size == 0)
+    {
+      return false;
+    }
+
+    _size--;
+
+    if (_frontPosition == _capacity - 1)
+    {
+      _frontPosition = 0;
+    }
+    else
+    {
+      _frontPosition++;
+    }
+
+    return true;
   }
 
   int Front()
@@ -28,6 +64,8 @@ public:
     {
       return -1;
     }
+
+    return _queue[_frontPosition];
   }
 
   int Rear()
@@ -36,6 +74,8 @@ public:
     {
       return -1;
     }
+
+    return _queue[_rearPosition];
   }
 
   bool isEmpty()
@@ -51,6 +91,28 @@ public:
 private:
   int _capacity;
   int _size;
-  int _position;
+  int _rearPosition;
+  int _frontPosition;
   vector<int> _queue;
 };
+
+int main()
+{
+  MyCircularQueue circleQueue(3);
+
+  bool result1 = circleQueue.enQueue(1);  // true
+  bool result2 = circleQueue.enQueue(2);  // true
+  bool result3 = circleQueue.enQueue(3);  // true
+  bool result4 = circleQueue.enQueue(4);  // false
+  int result5 = circleQueue.Rear();       // 3
+  bool result6 = circleQueue.isFull();    // true
+  bool result7 = circleQueue.deQueue();   // true
+  bool result8 = circleQueue.enQueue(4);  // true
+  int result9 = circleQueue.Rear();       // 4
+  int result10 = circleQueue.Front();     // 2
+  bool result11 = circleQueue.enQueue(5); // false
+  bool result12 = circleQueue.deQueue();  // true
+  bool result13 = circleQueue.enQueue(5); // true
+  int result14 = circleQueue.Front();     // 3
+  int result15 = circleQueue.Rear();      // 5
+}
