ce6f547721fc4aa004cdf9513214ec9194ef75c0
William Sanders <william.sanders076@gmail.com>
2022-07-14 06:53:16 -0400

    NumberSubArraysSizeKAverageGreaterEqualThreshold CPP wip

---
+++ NumberSubArraysSizeKAverageGreaterEqualThreshold/CPP/NumberSubArraysSizeKAverageGreaterEqualThreshold.cpp
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+  int numOfSubarrays(vector<int> &arr, int k, int threshold)
+  {
+  }
+};
