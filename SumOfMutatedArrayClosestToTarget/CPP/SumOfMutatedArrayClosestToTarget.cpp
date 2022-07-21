cb03b1e5fb2ca4f6c21890ce09925f0ea06419bb
William Sanders <william.sanders076@gmail.com>
2022-07-21 05:45:08 -0400

    SumOfMutatedArrayClosestToTarget CPP wip

---
+++ SumOfMutatedArrayClosestToTarget/CPP/SumOfMutatedArrayClosestToTarget.cpp
@@ -0,0 +1,11 @@
+// https://leetcode.com/problems/sum-of-mutated-array-closest-to-target/
+#include <vector>
+
+using namespace std;
+class Solution
+{
+public:
+  int findBestValue(vector<int> &arr, int target)
+  {
+  }
+};
