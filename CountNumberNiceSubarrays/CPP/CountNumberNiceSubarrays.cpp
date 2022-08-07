c3ff79b74538ffca31a9711c549a13f5aa0a2b27
William Sanders <william.sanders076@gmail.com>
2022-08-07 16:19:17 -0400

    CountNumberNiceSubarrays CPP wip

---
+++ CountNumberNiceSubarrays/CPP/CountNumberNiceSubarrays.cpp
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/count-number-of-nice-subarrays/
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+  int numberOfSubarrays(vector<int> &nums, int k)
+  {
+  }
+};
