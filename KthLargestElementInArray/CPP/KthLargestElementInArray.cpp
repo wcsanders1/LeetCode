72afa04b015d4d4d3b984a156b19b6e256317db0
William Sanders <william.sanders076@gmail.com>
2022-08-13 10:12:12 -0400

    KthLargestElementInArray CPP solution

--- KthLargestElementInArray/CPP/KthLargestElementInArray.cpp
+++ KthLargestElementInArray/CPP/KthLargestElementInArray.cpp
@@ -1,5 +1,6 @@
 // https://leetcode.com/problems/kth-largest-element-in-an-array/
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -8,5 +9,16 @@ class Solution
 public:
   int findKthLargest(vector<int> &nums, int k)
   {
+    auto midptr = nums.end() - k;
+    nth_element(nums.begin(), midptr, nums.end());
+    return *midptr;
   }
 };
+
+int main()
+{
+  Solution solution;
+
+  int result1 = solution.findKthLargest(*new vector<int>{3, 2, 1, 5, 6, 4}, 2);
+  int result2 = solution.findKthLargest(*new vector<int>{3, 2, 3, 1, 2, 4, 5, 5, 6}, 4);
+}
