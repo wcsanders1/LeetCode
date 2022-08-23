63f68ad9a338e0e09badc40a1021cb71e11428bd
William Sanders <william.sanders076@gmail.com>
2022-08-23 06:50:43 -0400

    RevealCardsIncreasingOrder CPP solution

--- RevealCardsIncreasingOrder/CPP/RevealCardsIncreasingOrder.cpp
+++ RevealCardsIncreasingOrder/CPP/RevealCardsIncreasingOrder.cpp
@@ -1,6 +1,7 @@
 // https://leetcode.com/problems/reveal-cards-in-increasing-order/
+// NOT MINE: https://leetcode.com/problems/reveal-cards-in-increasing-order/discuss/201574/C%2B%2B-with-picture-skip-over-empty-spaces
 #include <vector>
-#include <queue>
+#include <algorithm>
 
 using namespace std;
 
@@ -9,35 +10,19 @@ class Solution
 public:
   vector<int> deckRevealedIncreasing(vector<int> &deck)
   {
-    vector<int> answer(deck.size(), 0);
-    priority_queue<int, vector<int>, greater<int>> pq;
-
-    for (int card : deck)
+    sort(begin(deck), end(deck));
+    vector<int> res(deck.size(), 0);
+    res[0] = deck[0];
+    for (auto i = 1, p = 0; i < deck.size(); ++i)
     {
-      pq.push(card);
-    }
-
-    for (int i = 0; i < deck.size(); i += 2)
-    {
-      answer[i] = pq.top();
-      pq.pop();
-    }
-
-    int position = deck.size() % 2 == 0 ? 1 : 3;
-    while (!pq.empty())
-    {
-      if (position >= deck.size())
+      for (auto j = 0; j < 2; p %= res.size(), j += (res[p] == 0 ? 1 : 0))
       {
-        position = position - (deck.size() - 1) - (deck.size() % 2 == 0 ? 1 : 0);
+        ++p;
       }
 
-      answer[position] = pq.top();
-      pq.pop();
-
-      position += 4;
+      res[p] = deck[i];
     }
-
-    return answer;
+    return res;
   }
 };
 
@@ -48,5 +33,8 @@ int main()
   auto result1 = solution.deckRevealedIncreasing(*new vector<int>{17, 13, 11, 2, 3, 5, 7});
   auto result2 = solution.deckRevealedIncreasing(*new vector<int>{17, 13, 11, 2, 3, 5});
   auto result3 = solution.deckRevealedIncreasing(*new vector<int>{1, 1000});
-  auto result4 = solution.deckRevealedIncreasing(*new vector<int>{1, 2, 3, 4});
+  auto result4 = solution.deckRevealedIncreasing(*new vector<int>{1, 2, 3, 4}); // 1, 3, 2, 4
+  auto result5 = solution.deckRevealedIncreasing(*new vector<int>{1});
+  auto result6 = solution.deckRevealedIncreasing(*new vector<int>{1, 2, 3});
+  auto result7 = solution.deckRevealedIncreasing(*new vector<int>{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15});
 }
