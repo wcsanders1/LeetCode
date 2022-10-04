1c67e4cc94cb5b8f9281c535356eb3e1d8a51daa
William Sanders <william.sanders076@gmail.com>
2022-10-04 06:01:16 -0400

    ClosestRoom CPP wip

--- ClosestRoom/CPP/ClosestRoom.cpp
+++ ClosestRoom/CPP/ClosestRoom.cpp
@@ -63,7 +63,13 @@ public:
     for (int i = 1; i < sizes.size(); i++)
     {
       RoomSize prev = sizes[i - 1];
-      vector<int> v = vector<int>(prev.idsAtLeastThisSize.begin() + prev.idsThisSize.size(), prev.idsAtLeastThisSize.end());
+      vector<int> v = vector<int>(prev.idsAtLeastThisSize.begin(), prev.idsAtLeastThisSize.end());
+      for (int id : prev.idsThisSize)
+      {
+        int index = getIndex(v, id, 0, v.size() - 1);
+        v.erase(v.begin() + index);
+      }
+
       sizes[i].idsAtLeastThisSize = v;
     }
 
@@ -74,13 +80,23 @@ public:
       int bestId = q[0];
       int minSize = q[1];
 
-      int sizeIndex = getIndex(sizes, minSize, 0, sizes.size());
+      int sizeIndex = getIndex(sizes, minSize, 0, sizes.size() - 1);
+      if (sizeIndex == -1)
+      {
+        continue;
+      }
+
       if (sizes[sizeIndex].size < minSize)
       {
         continue;
       }
 
-      int idIndex = getIndex(sizes[sizeIndex].idsAtLeastThisSize, bestId, 0, sizes[sizeIndex].idsAtLeastThisSize.size());
+      int idIndex = getIndex(sizes[sizeIndex].idsAtLeastThisSize, bestId, 0, sizes[sizeIndex].idsAtLeastThisSize.size() - 1);
+      if (idIndex == -1)
+      {
+        continue;
+      }
+
       answer[i] = sizes[sizeIndex].idsAtLeastThisSize[idIndex];
     }
 
@@ -90,9 +106,14 @@ public:
 private:
   int getIndex(vector<int> &ints, int value, int start, int end)
   {
-    if (start < end)
+    if (start > end)
     {
-      return 0;
+      return -1;
+    }
+
+    if (start == end)
+    {
+      return start;
     }
 
     int mid = (start + end) / 2;
@@ -102,7 +123,7 @@ private:
       return mid;
     }
 
-    if (v < value)
+    if (v > value)
     {
       return getIndex(ints, value, start, mid - 1);
     }
@@ -112,9 +133,14 @@ private:
 
   int getIndex(vector<RoomSize> &sizes, int value, int start, int end)
   {
-    if (start < end)
+    if (start > end)
     {
-      return 0;
+      return -1;
+    }
+
+    if (start == end)
+    {
+      return start;
     }
 
     int mid = (start + end) / 2;
@@ -124,7 +150,7 @@ private:
       return mid;
     }
 
-    if (v.size < value)
+    if (v.size > value)
     {
       return getIndex(sizes, value, start, mid - 1);
     }
@@ -137,6 +163,6 @@ int main()
 {
   Solution solution;
 
-  auto result1 = solution.closestRoom(*new vector<vector<int>>{{2, 2}, {1, 2}, {3, 2}}, *new vector<vector<int>>{{3, 1}, {3, 3}, {5, 2}});
+  // auto result1 = solution.closestRoom(*new vector<vector<int>>{{2, 2}, {1, 2}, {3, 2}}, *new vector<vector<int>>{{3, 1}, {3, 3}, {5, 2}});
   auto result2 = solution.closestRoom(*new vector<vector<int>>{{1, 4}, {2, 3}, {3, 5}, {4, 1}, {5, 2}}, *new vector<vector<int>>{{2, 3}, {2, 4}, {2, 5}});
 }
