39916ec7a85ac86df5c8bfa82d2253835f822c3b
William Sanders <william.sanders076@gmail.com>
2022-10-10 07:00:16 -0400

    OpenTheLock CPP wip

---
+++ OpenTheLock/CPP/OpenTheLock.cpp
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/open-the-lock/
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Solution
+{
+public:
+  int openLock(vector<string> &deadends, string target)
+  {
+  }
+};
