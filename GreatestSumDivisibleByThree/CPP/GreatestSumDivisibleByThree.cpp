f29a410e0b085fdbdfcb87e6d1890c02b783c4df
William Sanders <william.sanders076@gmail.com>
2022-10-21 08:47:15 -0400

    GreatestSumDivisibleByThree CPP wip

---
+++ GreatestSumDivisibleByThree/CPP/GreatestSumDivisibleByThree.cpp
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/greatest-sum-divisible-by-three/
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+  int maxSumDivThree(vector<int> &nums)
+  {
+  }
+};
