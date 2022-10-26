eccbb1e8ab3979a71dd28bf73c50b20544e7d97e
William Sanders <william.sanders076@gmail.com>
2022-10-26 06:46:52 -0400

    MinimumCostToCutStick CPP wip

---
+++ MinimumCostToCutStick/CPP/MinimumCostToCutStick.cpp
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/minimum-cost-to-cut-a-stick/
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+  int minCost(int n, vector<int> &cuts)
+  {
+  }
+};
