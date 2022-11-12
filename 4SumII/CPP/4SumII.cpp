02030c4330ed1dd0f93950b55fa2aec9c55cd4eb
William Sanders <william.sanders076@gmail.com>
2022-11-12 17:46:12 -0500

    4SumII CPP solution

---
+++ 4SumII/CPP/4SumII.cpp
@@ -0,0 +1,57 @@
+// https://leetcode.com/problems/4sum-ii/
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution
+{
+public:
+  int fourSumCount(vector<int> &nums1, vector<int> &nums2, vector<int> &nums3, vector<int> &nums4)
+  {
+    int size = nums1.size();
+    vector<int> sums = vector<int>(size * size, 0);
+    for (int i = 0; i < size; i++)
+    {
+      for (int j = 0; j < size; j++)
+      {
+        int index = (i * size) + j;
+        sums[index] = nums1[i] + nums2[j];
+      }
+    }
+
+    unordered_map<int, int> nums;
+    for (int i = 0; i < size; i++)
+    {
+      for (int j = 0; j < size; j++)
+      {
+        int sum = nums3[i] + nums4[j];
+        if (nums.count(sum) == 0)
+        {
+          nums[sum] = 0;
+        }
+        nums[sum]++;
+      }
+    }
+
+    int answer = 0;
+    for (int s : sums)
+    {
+      int z = s * -1;
+      if (nums.count(z) != 0)
+      {
+        answer += nums[z];
+      }
+    }
+
+    return answer;
+  }
+};
+
+int main()
+{
+  Solution solution;
+
+  int result1 = solution.fourSumCount(*new vector<int>{1, 2}, *new vector<int>{-2, -1}, *new vector<int>{-1, 2}, *new vector<int>{0, 2});
+  int result2 = solution.fourSumCount(*new vector<int>{0}, *new vector<int>{0}, *new vector<int>{0}, *new vector<int>{0});
+}
