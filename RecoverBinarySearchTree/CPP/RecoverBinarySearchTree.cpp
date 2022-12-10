24ce14472efacf4927b1bb6ea91065ca26ba5c25
William Sanders <william.sanders076@gmail.com>
2022-12-10 14:39:17 -0500

    RecoverBinarySearchTree CPP wip

---
+++ RecoverBinarySearchTree/CPP/RecoverBinarySearchTree.cpp
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/recover-binary-search-tree/description/
+#include <vector>
+
+using namespace std;
+
+struct TreeNode
+{
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+  void recoverTree(TreeNode *root)
+  {
+  }
+};
