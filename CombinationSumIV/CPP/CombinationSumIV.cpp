4bc36e40d9a64b96ad98a666bc8fe7ba287f6eeb
William Sanders <william.sanders076@gmail.com>
2022-12-20 05:52:54 -0500

    CombinationSumIV CPP solution

--- CombinationSumIV/CPP/CombinationSumIV.cpp
+++ CombinationSumIV/CPP/CombinationSumIV.cpp
@@ -1,4 +1,5 @@
-//  https://leetcode.com/problems/combination-sum-iv/description/
+// https://leetcode.com/problems/combination-sum-iv/description/
+// NOT MINE: https://leetcode.com/problems/combination-sum-iv/solutions/85036/1ms-java-dp-solution-with-detailed-explanation/?orderBy=most_votes
 #include <vector>
 #include <algorithm>
 
@@ -9,36 +10,30 @@ class Solution
 public:
   int combinationSum4(vector<int> &nums, int target)
   {
-    sort(nums.begin(), nums.end());
-    vector<int> dp(1001, 0);
+    vector<int> dp(target + 1, -1);
+    dp[0] = 1;
+    return getCombinations(nums, target, dp);
+  }
 
-    if (nums[nums.size() - 1] < target)
+private:
+  int getCombinations(vector<int> &nums, int target, vector<int> &dp)
+  {
+    if (dp[target] != -1)
     {
-      nums.push_back(target);
+      return dp[target];
     }
 
-    dp[nums[0]] = 1;
-    for (int i = 1; i < nums.size(); i++)
+    int answer = 0;
+    for (int i = 0; i < nums.size(); i++)
     {
-      int num = nums[i];
-      dp[num] = 1;
-
-      vector<bool> visited(1001, false);
-
-      for (int j = i - 1; j >= 0; j--)
+      if (target >= nums[i])
       {
-        int res = num % nums[j];
-        int a = visited[nums[j]] ? 0 : dp[nums[j]];
-        int b = visited[res] ? 0 : dp[res];
-
-        dp[num] += a + b;
-
-        visited[nums[j]] = true;
-        visited[res] = true;
+        answer += getCombinations(nums, target - nums[i], dp);
       }
     }
 
-    return dp[target];
+    dp[target] = answer;
+    return answer;
   }
 };
 
@@ -46,6 +41,7 @@ int main()
 {
   Solution solution;
 
-  int result1 = solution.combinationSum4(*new vector<int>{1, 2, 3}, 4);
-  int result2 = solution.combinationSum4(*new vector<int>{9}, 3);
+  int result1 = solution.combinationSum4(*new vector<int>{1, 2, 3}, 4);                                                                              // 7
+  int result2 = solution.combinationSum4(*new vector<int>{9}, 3);                                                                                    // 0
+  int result3 = solution.combinationSum4(*new vector<int>{3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25}, 10); // 9
 }
