f34d88f85f461f5f1366a75c15911728f2f2f1c4
William Sanders <william.sanders076@gmail.com>
2023-01-02 12:41:28 -0500

    CanIWin CPP not done

---
+++ CanIWin/CPP/CanIWin.cpp
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/can-i-win/description/
+// NOT DONE
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+  bool canIWin(int maxChoosableInteger, int desiredTotal)
+  {
+    if (desiredTotal < maxChoosableInteger)
+    {
+      return true;
+    }
+  }
+};
