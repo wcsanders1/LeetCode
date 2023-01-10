112c3b6ea0a248d57a1ed58570a1492d47886ab1
William Sanders <william.sanders076@gmail.com>
2023-01-10 05:43:37 -0500

    ChampagneTower CPP solution

--- ChampagneTower/CPP/ChampagneTower.cpp
+++ ChampagneTower/CPP/ChampagneTower.cpp
@@ -1,52 +1,31 @@
 // https://leetcode.com/problems/champagne-tower/description/
+// NOT MINE: https://leetcode.com/problems/champagne-tower/solutions/127430/champagne-tower/?orderBy=most_votes
 #include <vector>
 
 using namespace std;
-
-struct Glass
-{
-  double increaseInterval;
-  double fullness;
-  Glass(double i, double f) : increaseInterval(i), fullness(f) {}
-};
 
 class Solution
 {
 public:
   double champagneTower(int poured, int query_row, int query_glass)
   {
-    vector<Glass> glasses;
-    vector<int> rowStart;
-    glasses.push_back(Glass(1, 0));
-    int start = 0;
-    int rowSize = 1;
-    int rows = query_row;
+    vector<vector<double>> dp(query_row + 2, vector<double>(query_row + 2, 0));
+    dp[0][0] = poured;
 
-    while (rows-- >= 0)
+    for (int r = 0; r <= query_row; r++)
     {
-      rowStart.push_back(start);
-      for (int i = 0; i <= rowSize; i++)
+      for (int c = 0; c <= r; c++)
       {
-        double increaseInterval = (i == 0 || i == rowSize)
-                                      ? glasses[start].increaseInterval / 2
-                                      : glasses[start].increaseInterval;
-
-        glasses.push_back(Glass(increaseInterval, 0));
-      }
-      start += ++rowSize;
-    }
-
-    while (poured-- > 0)
-    {
-      double glass = 1;
-      while (glass > 0)
-      {
+        double amount = (dp[r][c] - 1.0) / 2.0;
+        if (amount > 0)
+        {
+          dp[r + 1][c] += amount;
+          dp[r + 1][c + 1] += amount;
+        }
       }
     }
 
-    int index = rowStart[query_row] + (query_glass - 1);
-
-    return glasses[index].fullness;
+    return min(1.0, dp[query_row][query_glass]);
   }
 };
 
@@ -54,7 +33,17 @@ int main()
 {
   Solution solution;
 
-  // double result1 = solution.champagneTower(1, 1, 1);
-  // double result2 = solution.champagneTower(2, 1, 1);
-  double result3 = solution.champagneTower(100000009, 33, 17);
+  double result1 = solution.champagneTower(1, 1, 1);           // 0
+  double result2 = solution.champagneTower(2, 1, 1);           // .5
+  double result3 = solution.champagneTower(100000009, 33, 17); // 1
+  double result4 = solution.champagneTower(4, 1, 0);
+  double result5 = solution.champagneTower(4, 1, 1);
+  double result6 = solution.champagneTower(4, 2, 0);
+  double result7 = solution.champagneTower(4, 2, 1);
+  double result8 = solution.champagneTower(4, 2, 2);
+  double result9 = solution.champagneTower(5, 2, 0);
+  double result10 = solution.champagneTower(5, 2, 1);
+  double result11 = solution.champagneTower(5, 2, 2);
+  double result12 = solution.champagneTower(0, 0, 0);
+  double result13 = solution.champagneTower(25, 6, 1); // .1875
 }
