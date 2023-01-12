88399fc0afa1249c24f8ef8003144e08712c8b5f
William Sanders <william.sanders076@gmail.com>
2023-01-12 06:51:47 -0500

    CanIWin CPP solution

--- CanIWin/CPP/CanIWin.cpp
+++ CanIWin/CPP/CanIWin.cpp
@@ -1,6 +1,7 @@
 // https://leetcode.com/problems/can-i-win/description/
-// NOT DONE
+// NOT MINE: https://leetcode.com/problems/can-i-win/solutions/95320/clean-c-beat-98-4-dfs-with-early-termination-check-detailed-explanation/?orderBy=most_votes
 #include <vector>
+#include <unordered_map>
 
 using namespace std;
 
@@ -9,9 +10,60 @@ class Solution
 public:
   bool canIWin(int maxChoosableInteger, int desiredTotal)
   {
-    if (desiredTotal < maxChoosableInteger)
+    if (desiredTotal < 2)
     {
       return true;
     }
+
+    int sum = (maxChoosableInteger * (maxChoosableInteger + 1)) / 2;
+    if (sum < desiredTotal)
+    {
+      return false;
+    }
+
+    if (sum == desiredTotal)
+    {
+      return maxChoosableInteger % 2 == 1;
+    }
+
+    unordered_map<int, bool> stateMap;
+    return dfs(maxChoosableInteger, desiredTotal, 0, stateMap);
+  }
+
+private:
+  bool dfs(int maxChooseableInteger, int desiredTotal, int state, unordered_map<int, bool> &stateMap)
+  {
+    if (stateMap.count(state) != 0)
+    {
+      return stateMap[state];
+    }
+
+    if (desiredTotal <= 0)
+    {
+      return false;
+    }
+
+    for (int i = 0; i < maxChooseableInteger; i++)
+    {
+      int num = 1 << i;
+      if (!(state & num) && !dfs(maxChooseableInteger, desiredTotal - i - 1, state | num, stateMap))
+      {
+        stateMap[state] = true;
+        return true;
+      }
+    }
+
+    stateMap[state] = false;
+    return false;
   }
 };
+
+int main()
+{
+  Solution solution;
+
+  bool result1 = solution.canIWin(10, 11);
+  bool result2 = solution.canIWin(10, 0);
+  bool result3 = solution.canIWin(10, 1);
+  bool result4 = solution.canIWin(4, 6);
+}
