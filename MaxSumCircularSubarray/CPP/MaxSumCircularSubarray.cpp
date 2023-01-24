a2479565b58ef13bb85d8cbae7df6e7269fc3bc8
William Sanders <william.sanders076@gmail.com>
2023-01-24 06:01:59 -0500

    MaxSumCircularSubarray CPP solution

--- MaxSumCircularSubarray/CPP/MaxSumCircularSubarray.cpp
+++ MaxSumCircularSubarray/CPP/MaxSumCircularSubarray.cpp
@@ -1,4 +1,5 @@
 // https://leetcode.com/problems/maximum-sum-circular-subarray/description/
+// NOT MINE: https://leetcode.com/problems/maximum-sum-circular-subarray/solutions/178422/one-pass/?orderBy=most_votes
 #include <vector>
 
 using namespace std;
@@ -8,61 +9,22 @@ class Solution
 public:
   int maxSubarraySumCircular(vector<int> &nums)
   {
-    int answer = nums[0];
-    int n = nums.size();
-    if (n == 1)
+    int curMax = 0;
+    int maxSum = nums[0];
+    int curMin = 0;
+    int minSum = nums[0];
+    int total = 0;
+
+    for (int n : nums)
     {
-      return answer;
+      curMax = max(curMax + n, n);
+      maxSum = max(curMax, maxSum);
+      curMin = min(curMin + n, n);
+      minSum = min(curMin, minSum);
+      total += n;
     }
 
-    int start = 0;
-    int end = 0;
-    int cur = nums[0];
-    nums.insert(nums.end(), nums.begin(), nums.end());
-
-    while (end < nums.size())
-    {
-      while (end - start >= n - 1)
-      {
-        cur -= nums[start++];
-      }
-      answer = max(answer, cur);
-
-      while (nums[start] <= 0 && end < nums.size())
-      {
-        cur -= nums[start++];
-        if (start > end)
-        {
-          if (end >= nums.size() - 1)
-          {
-            break;
-          }
-          cur += nums[++end];
-          answer = max(answer, cur);
-        }
-      }
-
-      if (end < nums.size() - 1)
-      {
-        end++;
-        if (nums[end] >= cur + nums[end])
-        {
-          cur = nums[end];
-          start = end;
-        }
-        else
-        {
-          cur += nums[end];
-        }
-        answer = max(answer, cur);
-      }
-      else
-      {
-        end++;
-      }
-    }
-
-    return answer;
+    return maxSum > 0 ? max(maxSum, total - minSum) : maxSum;
   }
 };
 
@@ -70,10 +32,11 @@ int main()
 {
   Solution solution;
 
-  int result1 = solution.maxSubarraySumCircular(*new vector<int>{1, -2, 3, -2});              // 3
-  int result2 = solution.maxSubarraySumCircular(*new vector<int>{5, -3, 5});                  // 10
-  int result3 = solution.maxSubarraySumCircular(*new vector<int>{-3, -2, -3});                // -2
-  int result4 = solution.maxSubarraySumCircular(*new vector<int>{2, 2});                      // 4
-  int result5 = solution.maxSubarraySumCircular(*new vector<int>{2});                         // 2
-  int result6 = solution.maxSubarraySumCircular(*new vector<int>{0, 5, 8, -9, 9, -7, 3, -2}); // 16
+  int result1 = solution.maxSubarraySumCircular(*new vector<int>{1, -2, 3, -2});                  // 3
+  int result2 = solution.maxSubarraySumCircular(*new vector<int>{5, -3, 5});                      // 10
+  int result3 = solution.maxSubarraySumCircular(*new vector<int>{-3, -2, -3});                    // -2
+  int result4 = solution.maxSubarraySumCircular(*new vector<int>{2, 2});                          // 4
+  int result5 = solution.maxSubarraySumCircular(*new vector<int>{2});                             // 2
+  int result6 = solution.maxSubarraySumCircular(*new vector<int>{0, 5, 8, -9, 9, -7, 3, -2});     // 16
+  int result7 = solution.maxSubarraySumCircular(*new vector<int>{-5, -2, 5, 6, -2, -7, 0, 2, 8}); // 14
 }
