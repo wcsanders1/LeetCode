a3c3061f728fea1dc8e78c7c2b90a44536f08176
William Sanders <william.sanders076@gmail.com>
2023-01-31 05:53:01 -0500

    SnakesAndLadders CPP solution

--- SnakesAndLadders/CPP/SnakesAndLadders.cpp
+++ SnakesAndLadders/CPP/SnakesAndLadders.cpp
@@ -1,7 +1,7 @@
 // https://leetcode.com/problems/snakes-and-ladders/description/
+// NOT MINE: https://leetcode.com/problems/snakes-and-ladders/solutions/173403/change-to-1d-array-then-bfs/?orderBy=most_votes
 #include <vector>
-#include <unordered_map>
-#include <set>
+#include <queue>
 
 using namespace std;
 
@@ -11,7 +11,11 @@ public:
   int snakesAndLadders(vector<vector<int>> &board)
   {
     vector<int> longBoard;
+    int boardSize = board.size() * board.size();
     bool right = true;
+    vector<bool> visited(boardSize + 1, false);
+
+    longBoard.push_back(0);
     for (int row = board.size() - 1; row >= 0; row--)
     {
       if (right)
@@ -31,45 +35,48 @@ public:
       right = !right;
     }
 
-    unordered_map<int, int> cache;
-    set<int> visited;
+    queue<int> q;
+    q.push(1);
+    visited[1] = true;
+    int count = 1;
 
-    return getLeastSteps(0, cache, longBoard, visited);
-  }
-
-private:
-  int getLeastSteps(int square, unordered_map<int, int> &cache, vector<int> &longBoard, set<int> &visited)
-  {
-    if (square >= longBoard.size() - 1)
+    while (!q.empty())
     {
-      return 0;
-    }
-
-    if (square >= longBoard.size() - 7)
-    {
-      return 1;
-    }
-
-    if (cache.count(square) > 0)
-    {
-      return cache[square];
-    }
-
-    int least = (longBoard.size() - square + 1) / 6;
-    for (int i = square + 1; i <= square + 6; i++)
-    {
-      int next = longBoard[i] == -1 ? i : longBoard[i] - 1;
-      if (visited.count(next) > 0)
+      int moves = q.size();
+      for (int move = 0; move < moves; move++)
       {
-        continue;
+        int square = q.front();
+        q.pop();
+        if (square == boardSize)
+        {
+          return count;
+        }
+
+        for (int i = 1; i <= 6; i++)
+        {
+          int next = square + i;
+          if (longBoard[next] != -1)
+          {
+            next = longBoard[next];
+          }
+
+          if (next == boardSize)
+          {
+            return count;
+          }
+
+          if (visited[next])
+          {
+            continue;
+          }
+          visited[next] = true;
+          q.push(next);
+        }
       }
-      visited.insert(next);
-      least = min(least, getLeastSteps(next, cache, longBoard, visited) + 1);
+      count++;
     }
 
-    cache[square] = least;
-
-    return least;
+    return -1;
   }
 };
 
@@ -83,8 +90,12 @@ int main()
       {-1, -1, -1, -1, -1, -1},
       {-1, 35, -1, -1, 13, -1},
       {-1, -1, -1, -1, -1, -1},
-      {-1, 15, -1, -1, -1, -1}});
+      {-1, 15, -1, -1, -1, -1}}); // 4
   int result2 = solution.snakesAndLadders(*new vector<vector<int>>{
       {-1, -1},
-      {-1, 3}});
+      {-1, 3}}); // 1
+  int result3 = solution.snakesAndLadders(*new vector<vector<int>>{
+      {-1, 4, -1},
+      {6, 2, 6},
+      {-1, 3, -1}}); // 2
 }
