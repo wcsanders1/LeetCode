4227b6530daf780c82597f6f28ec63fe54be887b
William Sanders <william.sanders076@gmail.com>
2023-02-24 06:02:47 -0500

    RemoveKDigits CPP solution

--- RemoveKDigits/CPP/RemoveKDigits.cpp
+++ RemoveKDigits/CPP/RemoveKDigits.cpp
@@ -11,5 +11,65 @@ class Solution
 public:
   string removeKdigits(string num, int k)
   {
+    if (k == num.size())
+    {
+      return "0";
+    }
+
+    stack<char> s;
+    s.push(num[0]);
+
+    int i = 1;
+    for (; i < num.size() && k > 0; i++)
+    {
+      char n = num[i];
+      while (!s.empty() && n < s.top() && k > 0)
+      {
+        s.pop();
+        k--;
+      }
+      s.push(n);
+    }
+
+    for (; i < num.size(); i++)
+    {
+      s.push(num[i]);
+    }
+
+    while (k-- > 0 && s.size() > 1)
+    {
+      s.pop();
+    }
+
+    stack<char> r;
+    while (!s.empty())
+    {
+      r.push(s.top());
+      s.pop();
+    }
+
+    string answer;
+    while (r.size() > 1 && r.top() == '0')
+    {
+      r.pop();
+    }
+
+    while (!r.empty())
+    {
+      answer += r.top();
+      r.pop();
+    }
+
+    return answer;
   }
 };
+
+int main()
+{
+  Solution solution;
+
+  string result1 = solution.removeKdigits("1432219", 3); // 1219
+  string result2 = solution.removeKdigits("10200", 1);   // 200
+  string result3 = solution.removeKdigits("10", 2);      // 0
+  string result4 = solution.removeKdigits("112", 1);     // 11
+}
