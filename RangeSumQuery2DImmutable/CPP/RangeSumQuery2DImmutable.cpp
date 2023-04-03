ec4f0883d12dcfcb826ab83c4108044d1a718c2e
William Sanders <william.sanders076@gmail.com>
2023-04-03 06:34:21 -0400

    RangeSumQuery2DImmutable CPP solution

--- RangeSumQuery2DImmutable/CPP/RangeSumQuery2DImmutable.cpp
+++ RangeSumQuery2DImmutable/CPP/RangeSumQuery2DImmutable.cpp
@@ -15,48 +15,31 @@ public:
   {
     int rows = matrix.size();
     int cols = matrix[0].size();
-    sums = vector<vector<vector<vector<int>>>>(rows, vector<vector<vector<int>>>(cols, vector<vector<int>>(rows, vector<int>(cols))));
-    for (int startRow = 0; startRow < rows; startRow++)
+    sums = vector<vector<int>>(rows + 1, vector<int>(cols + 1, 0));
+
+    for (int row = 1; row <= rows; row++)
     {
-      for (int endRow = startRow; endRow < rows; endRow++)
+      int rowTotal = 0;
+      for (int col = 1; col <= cols; col++)
       {
-        for (int startCol = 0; startCol < cols; startCol++)
-        {
-          for (int endCol = startCol, rowTotal = 0; endCol < cols; endCol++)
-          {
-            rowTotal += matrix[endRow][endCol];
-            if (startRow == endRow)
-            {
-              if (startCol == endCol)
-              {
-                sums[startRow][startCol][endRow][endCol] = matrix[startRow][startCol];
-              }
-              else
-              {
-                sums[startRow][startCol][endRow][endCol] = sums[startRow][startCol][endRow][endCol - 1] + matrix[startRow][endCol];
-              }
-            }
-            else if (startCol == endCol)
-            {
-              sums[startRow][startCol][endRow][endCol] = sums[startRow][startCol][endRow - 1][endCol] + matrix[endRow][startCol];
-            }
-            else
-            {
-              sums[startRow][startCol][endRow][endCol] = sums[startRow][startCol][endRow - 1][endCol] + rowTotal;
-            }
-          }
-        }
+        rowTotal += matrix[row - 1][col - 1];
+        sums[row][col] = sums[row - 1][col] + rowTotal;
       }
     }
   }
 
   int sumRegion(int row1, int col1, int row2, int col2)
   {
-    return sums[row1][col1][row2][col2];
+    int total = sums[row2 + 1][col2 + 1];
+    int topRemove = sums[row1][col2 + 1];
+    int leftRemove = sums[row2 + 1][col1];
+    int dup = sums[row1][col1];
+
+    return total - ((topRemove + leftRemove) - dup);
   }
 
 private:
-  vector<vector<vector<vector<int>>>> sums;
+  vector<vector<int>> sums;
 };
 
 /**
@@ -67,14 +50,20 @@ private:
 
 int main()
 {
-  auto matrix = new NumMatrix(*new vector<vector<int>>{
+  auto matrix1 = new NumMatrix(*new vector<vector<int>>{
       {3, 0, 1, 4, 2},
       {5, 6, 3, 2, 1},
       {1, 2, 0, 1, 5},
       {4, 1, 0, 1, 7},
       {1, 0, 3, 0, 5}});
 
-  int result1 = matrix->sumRegion(2, 1, 4, 3);
-  int result2 = matrix->sumRegion(1, 1, 2, 2);
-  int result3 = matrix->sumRegion(1, 2, 2, 4);
+  int result1 = matrix1->sumRegion(2, 1, 4, 3);
+  int result2 = matrix1->sumRegion(1, 1, 2, 2);
+  int result3 = matrix1->sumRegion(1, 2, 2, 4);
+
+  auto matrix2 = new NumMatrix(*new vector<vector<int>>{{1}, {-7}});
+
+  int result4 = matrix2->sumRegion(0, 0, 0, 0);
+  int result5 = matrix2->sumRegion(1, 0, 1, 0);
+  int result6 = matrix2->sumRegion(0, 0, 1, 0);
 }
