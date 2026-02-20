f547416f6182fa3d8a053a04edb7d7738ab5feab
William Sanders <william.sanders076@gmail.com>
2026-02-20 06:03:07 -0500

    DeleteNodeInALinkedList CPP solution

---
+++ DeleteNodeInALinkedList/CPP/DeleteNodeInALinkedList.cpp
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/delete-node-in-a-linked-list/description/
+#include <vector>
+#include <unordered_set>
+#include <queue>
+#include <stack>
+#include <string>
+#include <unordered_map>
+
+using namespace std;
+
+struct ListNode
+{
+  int val;
+  ListNode *next;
+  ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution
+{
+public:
+  void deleteNode(ListNode *node)
+  {
+  }
+};
